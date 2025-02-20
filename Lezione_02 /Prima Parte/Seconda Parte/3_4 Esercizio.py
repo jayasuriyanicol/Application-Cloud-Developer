ae57f1d0fc3134329d1510326b2e27590d2d1864
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2025-02-20 22:42:40 +0100

    terzo commit

--- Lezione_02 /Prima Parte/Seconda Parte/3_4 Esercizio.py
+++ Lezione_02 /Prima Parte/Seconda Parte/3_4 Esercizio.py
@@ -4,3 +4,16 @@ Make a list that includes at least three people you’d like to invite to dinner
 Then use your list to print a message to each person, inviting them to dinner.
 
 '''
+
+invited=["Cristiano Ronaldo", "Will Smith", "Michael Jackson"] 
+
+#Metodo basilare, più semplice e inutivo, ma allo stesso tempo meccanico
+
+message=print(f"Welcome {invited[0] }, you are invited to my dinner !")
+message=print(f"Welcome {invited[1] }, you are invited to my dinner !")
+message=print(f"Welcome {invited[2] }, you are invited to my dinner !\n")
+
+
+#Metodo più rapido con l'utilizzo del ciclo FOR
+for name in invited:
+     message=print(f"Welcome {name}, you are invited to my dinner !")
