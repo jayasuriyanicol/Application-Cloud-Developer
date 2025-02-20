5cc2840749c6e58d438e53b301b06d7a9357e872
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2025-02-20 22:23:17 +0100

    second commit

---
+++ Lezione_02 /Prima Parte/Seconda Parte/3_1 Esercizio.py
@@ -0,0 +1,21 @@
+'''
+3-1. Names: Store the names of a few of your friends in a list called names. 
+Print each person’s name by accessing each element in the list, one at a time.
+
+'''
+# METODO BASICO
+
+friends_name=["Nicol", "Nathan", "Michael"] 
+print(f"Ciao {friends_name[0]} come stai?")
+print(f"Ciao {friends_name[1]} come stai?")
+print(f"Ciao {friends_name[2]} come stai?")
+
+
+# ALTERNATIVA CON L'UTILIZZO DEL CICLO FOR
+
+'''for friend in friends_name:
+    print(f"Ciao {friend}, come stai?")'''
+
+
+
+
