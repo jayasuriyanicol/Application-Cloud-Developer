51ddb388be4d91c3952e94061fa9d176d3309a8c
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2025-03-09 16:42:35 +0100

    UPDATE - 09_03_2025

--- Lezione_04/8_8 Esercizio.py
+++ Lezione_04/8_8 Esercizio.py
@@ -1,6 +1,7 @@
 '''
-8-9. Messages: Make a list containing a series of short text messages. 
-Pass the list to a function called show_messages(), which prints each text message.
+8-8. User Albums: Start with your program from Exercise 8-7. Write a while loop that allows users to enter an album’s artist and title. 
+Once you have that information, call make_album() with the user’s input and print the dictionary that’s created. 
+Be sure to include a quit value in the while loop.
 '''
 
 #INIZIO ESERCIZIO NUMERO 8_7
