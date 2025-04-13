18f1b0645709becbfa54b1b7ad812c8b8dbcde3f
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2025-04-13 13:56:36 +0200

    UPDATE - 13_04_2025

---
+++ Python 1-4/Lezione_09/Esercizi sulle RegEx /Esercizi sulle RegEx - Python/Esercizio_06.py
@@ -0,0 +1,20 @@
+'''
+6. Verifica un codice prodotto
+
+Scrivi una funzione check_product_code(code) che verifica se il codice è nel formato PROD-1234-AB.
+
+Esempio:
+
+check_product_code("PROD-9876-ZX")  # True
+check_product_code("PROD-99-ZX")    # False
+'''
+#Importiamo la funzione re.findall per la ricerca del codice prodotto che rispetta o standard ovvero (PROD-NNNN-AZ) attraverso una funzione BOOL
+import re
+
+def check_product_code(code:str) -> str:
+
+    return bool (re.findall(r'PROD-\d{4}-[A-Z]',code))
+
+#Andiamo a testare i vari codici, verificando che essi siano compatibili allo standard
+print(check_product_code ("PROD-9876-ZX"))
+print(check_product_code("PROD-99-ZX") )
