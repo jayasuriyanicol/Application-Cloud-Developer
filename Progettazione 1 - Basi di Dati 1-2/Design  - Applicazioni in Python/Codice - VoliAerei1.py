78a9b2d8a3ea1e23d4bbdfdb9ca6696f488df4ef
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2025-05-19 17:32:18 +0200

    UPDATE - 19_05_2025

--- Progettazione 1 - Basi di Dati 1-2/Design  - Applicazioni in Python/Codice - VoliAerei1.py
+++ Progettazione 1 - Basi di Dati 1-2/Design  - Applicazioni in Python/Codice - VoliAerei1.py
@@ -2,17 +2,18 @@
 Dopo aver effettuato la ristrutturazione dal Diagramma delle classi (UML) di 'VoliAerei1', possiamo procedere con la stesura del codice in linguaggio Pyhthon partendo dal diagramma risturtturato
 '''
 
-from typing import Self
+from typing import Self,Any
+
 
 '''Procediamo prima con i TIPI DI DATO SPECIALIZZATI, delle varire classi con VINCOLI.'''
 
-class MinutiDurata(int):
 
+class MinutiDurata(int):
     def __new__(cls, value: int) -> Self:
 
         if value <= 0:
 
-            raise ValueError("\nATTENZIONE!\nLa durata in minuti deve essere > 0\n")
+            raise ValueError("ATTENZIONE!\nLa durata in minuti deve essere > 0")
         
         return int.__new__(cls, value)
 
@@ -24,10 +25,11 @@ class NumeroAbitanti(int):
 
         if value < 0:
 
-            raise ValueError("\nATTENZIONE !\nIl numero di abitanti deve essere >= 0 non minore !\n")
+            raise ValueError("ATTENZIONE !\nIl numero di abitanti deve essere >= 0 non minore !")
         
         return int.__new__(cls, value)
     
+
 
 class AnnoFondazione(int):
 
@@ -35,111 +37,163 @@ class AnnoFondazione(int):
 
         if value <= 1900:
 
-            raise ValueError("\nATTENZIONE!\nLa data della fondazione deve essere >= all'anno 1900\n")
+            raise ValueError("ATTENZIONE!\nLa data della fondazione deve essere >= all'anno 1900")
         
         return int.__new__(cls, value)
 
 
+
 '''Infine procediamo con la stesura per le CLASSI CONCETTUALI del DIAGRAMMA RISTRUTTURATO'''
+
+
+
 
 #Definiamo la classe Aeroporto con attributi -> codice: str e nome:str
 class Aeroporto:
+
     def __init__(self, codice:str, nome:str)-> None:
+
         self.codice = codice
         self.nome = nome
 
-    def __eq__(self, other)-> bool:
-        return isinstance(other, Aeroporto) and self.codice == other.codice
+    def __eq__(self, other:Any)-> bool:
+
+        if other is None or not isinstance(other, type(self)) or hash(self) != hash(other):
+
+            return False
+        
+        return (self.codice) == (other.codice)
+
 
     def __hash__(self)-> int:
-        return hash(self.codice)
+
+        return hash((self.codice,))
 
     def __repr__(self)-> str:
-        return f"\nAEROPORTO ID: {self.codice} | NOME COMPLETO AEROPORTO: {self.nome}\n"
-    
+
+        return f"AEROPORTO ID:({self.codice} | NOME COMPLETO AEROPORTO: {self.nome})"
+
 
 
 
 #Definiamo la classe Città con i seguenti attributi -> nome: str, numeroAbitanti: int
 class Città:
+
     def __init__(self, nome:str, numeroAbitanti:int)-> None:
+
         self.nome = nome
         self.numeroAbitanti = NumeroAbitanti(numeroAbitanti)
 
-    def __eq__(self, other)-> bool:
-        return isinstance(other, Città) and self.nome == other.nome
+
+    def __eq__(self, other: Any) -> bool:
+
+        if other is None or not isinstance(other, type(self)) or hash(self) != hash(other):
+
+            return False
+        
+        return (self.nome) == (other.nome)
+    
 
     def __hash__(self)-> int:
-        return hash(self.nome)
+
+        return hash((self.nome))
 
     def __repr__(self)-> str:
-        return f"\nNOME COMPLETO DELLA CITTÀ: ({self.nome}, | NUMERO COMPLESSIVO ABITANTI: {self.numeroAbitanti}\n)"
 
-
+        return f"NOME COMPLETO DELLA CITTÀ: ({self.nome}, | NUMERO COMPLESSIVO ABITANTI: {self.numeroAbitanti})"
 
 
 #Definiamo la classe Nazione con i seguenti attributo -> nome: str
 class Nazione:
-    def __init__(self, nome:str)->None:
+
+    def __init__(self, nome:str) -> None:
+
         self.nome = nome
 
-    def __eq__(self, other)-> bool:
-        return isinstance(other, Nazione) and self.nome == other.nome
+    def __eq__(self, other: Any) -> bool:
+
+        if other is None or not isinstance(other, type(self)) or hash(self) != hash(other):
+
+            return False
+        
+        return (self.nome) == (other.nome)
 
     def __hash__(self)-> int:
-        return hash(self.nome)
+
+        return hash((self.nome))
+
 
     def __repr__(self)-> str:
-        return f"\nNOME COMPLETO DELLA NAZIONE: {self.nome}\n"
-    
-
+        return f"NOME COMPLETO DELLA NAZIONE: ({self.nome})"
 
 
 #Definiamo la classe CompagniaAerea con i seguenti attributi -> nome: str, fondazione: int
 class CompagniaAerea:
+
     def __init__(self, nome:str, fondazione:int) -> None:
+
         self.nome = nome
         self.fondazione = AnnoFondazione(fondazione)
 
-    def __eq__(self, other)-> bool:
-        return isinstance(other, CompagniaAerea) and self.nome == other.nome
+    def __eq__(self, other: Any) -> bool:
+
+        if other is None or not isinstance(other, type(self)) or hash(self) != hash(other):
+
+            return False
+        
+        return (self.nome) == (other.nome)
 
     def __hash__(self)-> int:
-        return hash(self.nome)
+
+        return hash((self.nome))
 
     def __repr__(self)-> str:
-        return f"\nNOME COMPLETO COMPAGNIA AEREA: ({self.nome}, | ANNO DELLA FONDAZIONE: {self.fondazione}\n)"
-    
 
+        return f"NOME COMPLETO COMPAGNIA AEREA: ({self.nome}, | ANNO DELLA FONDAZIONE: {self.fondazione})"
+    
 
 
 #Definiamo la classe Volo con i seguenti attributi -> codice: str, durataMinuti: int, arrivo: Oggetto della classe Aeroporto, partenza: Oggetto della classe Aeroporto, compagnia: Oggetto della classe CompagniaAerea
 class Volo:
+
     def __init__(self, codice:str, durataMinuti:int, partenza:Aeroporto , arrivo:Aeroporto , compagnia:CompagniaAerea)-> None:
+
         self.codice = codice
         self.durataMinuti = MinutiDurata(durataMinuti)
 
-        #Verifichiamo che siano effettivamente ISTANTE delle classi appartenenti per 'partenza,arrivo e compagnia'
+
+        #Verifichiamo che siano effettivamente ISTANZE delle classi appartenenti per 'partenza,arrivo e compagnia'
         if not isinstance(partenza, Aeroporto) or not isinstance(arrivo, Aeroporto):
-            raise TypeError("\nATTENZIONE!\nLa partenza e l'arrivo devono essere oggetti di tipo AEROPORTO\n")
+
+            raise TypeError("ATTENZIONE!\nLa partenza e l'arrivo devono essere oggetti di tipo AEROPORTO")
 
         if not isinstance(compagnia, CompagniaAerea):
-            raise TypeError("\nATTENZIONE!\nLa compagnia deve essere un oggetto di tipo COMPAGNIAAEREA\n")
+
+            raise TypeError("ATTENZIONE!\nLa compagnia deve essere un oggetto di tipo COMPAGNIAAEREA")
 
         self.partenza = partenza
         self.arrivo = arrivo
         self.compagnia = compagnia
 
-    def __eq__(self, other)-> bool:
-        return isinstance(other, Volo) and self.codice == other.codice
+
+    def __eq__(self, other: Any)-> bool:
+
+        if other is None or not isinstance(other, type(self)) or hash(self) != hash(other):
+
+            return False
+        
+        return (self.codice) == (other.codice)
+    
 
     def __hash__(self)-> int:
-        return hash(self.codice)
+
+        return hash((self.codice))
+
 
     def __repr__(self)-> str:
-        return (f"\n\nNUMERO VOLO: ({self.codice}| DURATA TOTALE IN MINUTI: {self.durataMinuti}\n"
-                f"AEROPORTO PARTENZA: {self.partenza}| AEROPORTO ARRIVO: {self.arrivo}| NOME COMPLETO COMPAGNIA: {self.compagnia}\n")
-    
+
+        return (f"NUMERO VOLO: ({self.codice}| DURATA TOTALE IN MINUTI: {self.durataMinuti}\n"
+                f"AEROPORTO PARTENZA: {self.partenza}| AEROPORTO ARRIVO: {self.arrivo}| NOME COMPLETO COMPAGNIA: {self.compagnia})")
 
 
 
