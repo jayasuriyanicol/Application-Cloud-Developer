077d4ef326d91ecbb4474b61f21bba50b261ba8d
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2025-05-21 19:37:44 +0200

    Esercizio - Quasi corretto mancanti commenti

--- Python 1_4/Lezione_11/Esercitazioni sulle Classi/1_0 Esercizio - Cinema.py
+++ Python 1_4/Lezione_11/Esercitazioni sulle Classi/1_0 Esercizio - Cinema.py
@@ -27,3 +27,74 @@ Test case:
 
 '''
 
+
+class Film:
+
+    def __init__(self, titolo: str, durata: int) -> None:
+        self.titolo = titolo
+        self.durata = durata
+
+
+class Sala:
+
+    def __init__(self, numeroIdentificativo: str, filmAttualeProgrammazione: Film, postiTotali: int) -> None:
+        self.numeroIdentificativo = numeroIdentificativo
+        self.filmAttualeProgrammazione = filmAttualeProgrammazione
+        self.postiTotali = postiTotali
+        self.postiPrenotati = 0
+
+    def prenota_posti(self, num_posti: int) -> str:
+
+        differenzaPostiDisponibili: int = self.postiTotali - self.postiPrenotati
+
+        if num_posti <= 0:
+            return f"ATTENZIONE! inserire un numero di posti coerente alla capienza della SALA !"
+
+        if num_posti <= differenzaPostiDisponibili:
+
+            self.postiPrenotati += num_posti
+
+            return f"SUCCESSO! sono stati prenotati {num_posti}"
+        
+        return f"ATTENZIONE ! sono rimanenti solo {differenzaPostiDisponibili} posti, non {num_posti} posti come richiesto !"
+
+    def posti_disponibili(self) -> str:
+
+        postiDisponibiliSala: int = self.postiTotali - self.postiPrenotati
+
+        return f"I POSTI disponibili nella sala sono {postiDisponibiliSala} posti !"
+
+
+class Cinema:
+
+    def __init__(self):
+        self.sale = []
+
+    def aggiungi_sala(self, sala: Sala) -> str:
+        self.sale.append(sala)
+        return f"SUCCESSO! È stata aggiunta una nuova sala con ID {sala.numeroIdentificativo} !"
+
+    def prenota_film(self, titolo_film: str, num_posti: int) -> str:
+
+        for sala in self.sale:
+            if sala.filmAttualeProgrammazione.titolo.lower() == titolo_film.lower():
+                return sala.prenota_posti(num_posti)
+
+        return f"ATTENZIONE ! per il FILM '{titolo_film}' sintassi o titolo del film ERRATO/NON ESISTENTE oppure non presente in NESSUNA SALA !"
+
+
+
+film1 = Film("Matrix", 120)
+sala1 = Sala("SAL001", film1, 100)
+
+cinema = Cinema()
+film1 = Film("Matrix", 120)
+sala1 = Sala(1, film1, 100)
+cinema = Cinema()
+
+print(f"\n{cinema.aggiungi_sala(sala1)}")
+
+print(f"\n{cinema.prenota_film('Matrix', 10)}")
+print(f"\n{cinema.prenota_film('Matrix', 95)}")  
+print(f"\n{sala1.posti_disponibili()}")
+
