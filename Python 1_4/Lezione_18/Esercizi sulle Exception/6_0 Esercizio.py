da58d455de56d02afd4f4a917488458791b7c181
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2025-05-22 09:35:58 +0200

    Creazione Esercizi - Non Svolti

---
+++ Python 1_4/Lezione_18/Esercizi sulle Exception/6_0 Esercizio.py
@@ -0,0 +1,12 @@
+'''
+6.Personalized math library: Create a Python library that provides functions for handling fractions, with built-in error handling. The library must include functions for the following operations:
+
+    Create a fraction from the numerator and denominator.
+    Collect the numerator and denominator of a fraction.
+    Simplify a fraction.
+    Add, subtract, multiply and divide fractions.
+    Check whether one fraction is equivalent to another. 
+
+    All library functions must use the try-except block to handle potential errors, such as null denominators, unsupported operations, or division by zero. The library must raise custom exceptions to indicate specific errors to the user.
+
+'''
