b5f9fcae16d9c660705911af7d83bb17e3fd1a2a
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2025-05-22 13:37:35 +0200

    UPDATE - 22_05_2025

---
+++ WEB 1/JavaScript/Esercizi su JavaScript/SLIDE 1 -  Esercizi su JavaScript/1_3 Esercizio/1_3 Esercizio.js
@@ -0,0 +1,20 @@
+/*Creare un progetto Javascript che dichiari e contestualmente inizializzi una variabile x = 10; che dichiari la variabile y e successivamente 
+assegni ad y il valore di x; che dichiari e contestualmente inizializzi la variabile z e gli assegna un nome di persona a proprio piacimento e 
+tramite output stampa il valore delle variabili ed il tipo. Eseguire l’esercizio nelle due modalità : lato client e lato server. */
+
+
+//Dichiariasmo e inizializziamo la x con il valore 10
+let x = 10; 
+//Dichiariamo la y           
+let y;           
+//Assegnamo il valore di y al valore di x      
+y = x;                 
+
+//Dichiarimao e inizializziamo il valore di z con un valore che è il nome 
+let z = "Luca";  
+       
+// Output in console
+console.log("Valori:");
+console.log("x =", x, "- tipo:", typeof x);
+console.log("y =", y, "- tipo:", typeof y);
+console.log("z =", z, "- tipo:", typeof z);
