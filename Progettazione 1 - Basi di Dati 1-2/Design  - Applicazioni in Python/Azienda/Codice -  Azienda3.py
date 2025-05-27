9a319f9f1145c57c2e8684ef95bd2dde22fb8399
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2025-05-27 16:09:43 +0200

    Code on working not still complete

--- Progettazione 1 - Basi di Dati 1-2/Design  - Applicazioni in Python/Azienda/Codice -  Azienda3.py
+++ Progettazione 1 - Basi di Dati 1-2/Design  - Applicazioni in Python/Azienda/Codice -  Azienda3.py
@@ -1,5 +1,8 @@
 from typing import Self
+from datetime import *
 
+
+'''Procediamo con lo sviluppo delle CLASSI che ci serviranno per lo sviluppo delle nostre CLASSI nel diagramma RISTRUTTURATO'''
 class RealGEZ(float):
     def __new__(cls, v:int|float|str) -> Self:
 
@@ -18,4 +21,67 @@ class RealGZ(RealGEZ):
         
         return RealGEZ.__new__(cls,v)
     
+
+'''Adesso, procediamo con la creazione e lo sviluppo delle classi dal DIAGRAMMA RISTRUTTURATO'''
+
+
+#Creiamo la classe IMPIEGATO della classe RISTRUTTURATA
+class Impiegato:
+
+    _nome:str
+    _cognome :str
+    _stipendioAnnuale : RealGZ
+    _dataAfferenza: datetime.date
+    _nascita : datetime.date
+    _dipartimento: "Dipartimento"
+
+    def nome(self)-> str:
+
+        return self._nome
     
+
+    def cognome(self) -> str:
+        return self._cognome
+    
+
+    def stipendioAnnuale(self)-> RealGZ:
+        return self._stipendioAnnuale
+    
+    def dataAfferenza(self) -> datetime.date:
+        return self._dataAfferenza
+    
+    def nascita(self)->datetime.date:
+
+        return self._nascita
+    
+
+    '''Successivamente, procediamo con la creazione dei Metodi GETTER e SETTER'''
+    def setNome(self, v:str) -> None:
+
+        self._nome:str = v
+    def setCognome(self, v:str) -> None:
+
+        self._cognome:str = v
+    def setstipendioAnnuale(self, v:str) -> None:
+
+        self._stipendioAnnuale:str = v
+
+    def setdataAfferenza(self, v:str) -> None:
+
+        self._dataAfferenza:str = v
+
+
+    '''NON PROCEDIAMO CON LA DEFINIZIONE DELLA FUNZIONE Nascita 'def setdaatNascita(self,v: ...) <<-- NO, perchè sappiamo che esso è <<immutable>> dalla nascita'''
+
+ 
+   #TO DO : Incompleto ! Manca l'inserimento del link 'afferenza' 
+    def __init__(self, nome:str, cognome:str, stipendioAnnuale:RealGZ, dataNascita: datetime.time, dataAfferenza: datetime.time):
+        
+        self.setNome(nome)
+        self.setCognome(cognome)
+        self.setstipendioAnnuale(stipendioAnnuale)
+        self.setdataAfferenza(dataAfferenza)
+        self.dataNascita = dataNascita 
+    
+
+
