c4efa6359b9d851fede1b8232690a317a75e639b
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2025-06-04 13:18:07 +0200

    Adding new exercise -  not complete

---
+++ Python 1_4/Recupero/Liste, Tuple,Dizionari/3_0 Esercizio.py
@@ -0,0 +1,9 @@
+'''
+3) Scrivi una funzione che accetti un dizionario di prodotti con i relativi prezzi e
+restituisca un nuovo dizionario con solo i prodotti che hanno un prezzo inferiore a 50, ma
+con i prezzi aumentati del 10% e arrotondati a due cifre decimali.
+
+'''
+
+
+def dizionarioConParametri (dict)
