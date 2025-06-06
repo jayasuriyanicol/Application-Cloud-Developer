006161078dda2407f2290ec199b32409009c7290
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2025-06-06 12:37:45 +0200

    UPDATE - 06_05_2025 | Moving files on new Folders

---
+++ Python 1_4/Recupero/Parte 1 -  Recupero/Funzioni /1_0 Esercizio.py
@@ -0,0 +1,28 @@
+'''
+Funzioni, For, While, If, Elif ed Else
+1) Scrivi una funzione che verifica se una combinazione di condizioni (X, Y, e Z) è
+soddisfatta per procedere con un'azione. L'azione può procedere solo se la condizione X
+è vera e almeno una tra Y e Z è vera. La funzione deve ritornare "Azione permessa"
+oppure "Azione negata" a seconda delle condizioni che sono soddisfatte.
+'''
+
+
+
+def X():
+    pass
+def Y():
+    pass
+def Z():
+    pass
+
+if X == True and Y == True or Z == True:
+    print("Azione Permessa")
+
+else:
+
+    print("Azione negata")
+
+
+
+
+
---
+++ Python 1_4/Recupero/Parte 1 -  Recupero/Funzioni /2_0 Esercizio.py
@@ -0,0 +1,5 @@
+'''
+Liste, Tuple e Dizionari
+2) Scrivi una funzione che prenda una lista di numeri e ritorni un dizionario che
+classifichi i numeri in liste separate per numeri positivi e negativi.
+'''
