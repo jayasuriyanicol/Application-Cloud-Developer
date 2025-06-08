d0b597a59e1fa0d06a1ca9c3b73e8cc816e00fba
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2025-06-08 09:48:54 +0200

    UPDATE - 08_06_2025 | New Adress IPv4 Validator

--- Python 1_4/Recupero/Parte 1 -  Recupero/Esercizi Restanti/Indirizzo IPv4.py
+++ Python 1_4/Recupero/Parte 1 -  Recupero/Esercizi Restanti/Indirizzo IPv4.py
@@ -20,42 +20,43 @@ is_valid_ipv4("192.168.1.a") # False (parte non numerica)
 '''
 
 
+#Definiamo una funzione che controlla se una stringa è un indirizzo IPv4 valido
 def is_valid_ipv4(address: str) -> bool:
 
+    #Verifichiamo che ci siano esattamente 3 punti nella stringa (IPv4 ha 4 blocchi)
     if address.count(".") != 3:
-
         return False
 
+    #Dividiamo la stringa in parti usando il punto come separatore
     partiDistinte = address.split(".")
 
-
+    #Controlliamo che il risultato della divisione abbia esattamente 4 elementi
     if len(partiDistinte) != 4:
-
         return False
     
+    #Per ogni parte dell’indirizzo
     for elemento in partiDistinte:
 
+        #Controlliamo che sia composta solo da cifre
         if not elemento.isdigit():
             return False
 
+        #Convertiamo la parte in intero per il controllo del range
         elementoCoerente = int(elemento)
 
+        #Verifichiamo che il valore sia compreso tra 0 e 255
         if not (0 <= elementoCoerente < 256):
-
-                return False
+            return False
     
+    #Se tutte le condizioni sono rispettate, l’indirizzo è valido
     return True
 
- 
 
+#DRIVER PROGRAM - Test delle funzionalità della funzione con vari casi
 print(is_valid_ipv4("192.168.0.1"))
 print(is_valid_ipv4("255.255.255.255"))
 print(is_valid_ipv4("256.100.10.1"))
 print(is_valid_ipv4("192.168.1"))
 print(is_valid_ipv4("192.168.1.a"))
-
-
-
-        
 
 
