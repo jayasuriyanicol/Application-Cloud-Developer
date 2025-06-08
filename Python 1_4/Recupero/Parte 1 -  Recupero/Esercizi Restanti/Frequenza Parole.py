48a19ff275a1a38b57fb7e9b204c4cc457d5aee6
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2025-06-08 09:47:15 +0200

    UPDATE - 08_06_2025 | New Excersise Word Frequency

--- Python 1_4/Recupero/Parte 1 -  Recupero/Esercizi Restanti/Frequenza Parole.py
+++ Python 1_4/Recupero/Parte 1 -  Recupero/Esercizi Restanti/Frequenza Parole.py
@@ -21,26 +21,29 @@ output = count_unique_words(text)
 
 '''
 
-
-
+#Importiamo dalla strind la funzione punctuation per eliminare relativi (?,.:,ecc.)
 from string import punctuation
 
 
+#Definiamo una funzione che calcola le occorrenzze (parole) che si ripetono in una determinata frase passata per input -> "testoConvertire"
 def occorrenzeFrase(testoConvertire:str)-> dict[str,int]:
 
     dizionarioOccorrenze:dict[str,int] = {}
 
-
+    #Splittiamo il testo in piccole parti -> "token" per verificare che ogni parola correttamente 
     tokens = testoConvertire.split()
 
     for token in tokens:
        
+       #Eliminiamo dal token la "punteggiatura" e nel caso sia nullo -> "stringa vuota" continuiamo
        elemento = token.lower().strip(punctuation)
 
        if elemento == '':
           
           continue
+       
 
+       #Per ogni elemento "token" spoglio da "impurita" (punteggiatura, testi vuoti, ecc.)  andiamo a verificare se già presente -> +1 se non presente -> 1
        if elemento in dizionarioOccorrenze:
        
            dizionarioOccorrenze[elemento] += 1 
@@ -50,7 +53,9 @@ def occorrenzeFrase(testoConvertire:str)-> dict[str,int]:
 
     return dizionarioOccorrenze
 
-    
+
+
+'''DRIVER PROGRAMM - Test delle funzionalità del programma'''
 
 text = "Hello, world! Hello... PYTHON? world."
 output = occorrenzeFrase(text)
