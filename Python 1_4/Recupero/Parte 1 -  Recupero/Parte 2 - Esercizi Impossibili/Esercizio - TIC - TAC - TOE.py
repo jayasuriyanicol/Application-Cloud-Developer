4cc543d9a75e29e2760ff882531056035ec0ed3e
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2025-06-10 19:14:44 +0200

    Adding Only prompt of - TIC-TAC-TOE

---
+++ Python 1_4/Recupero/Parte 1 -  Recupero/Parte 2 - Esercizi Impossibili/Esercizio - TIC - TAC - TOE.py
@@ -0,0 +1,8 @@
+'''
+1) Costruisci un algoritmo che permette di vincere sempre oppure pareggiare (nel caso peggiore) nel gioco tic-tac-toe.
+
+L'algoritmo si chiama minimax: https://en.wikipedia.org/wiki/Minimax
+
+Tic-tac-toe: https://en.wikipedia.org/wiki/Tic-tac-toe
+
+'''
