da03579f6196e29a2f9c4eb6e9b2d5aa389d5b99
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2025-06-10 19:16:02 +0200

    Adding Only prompt of - SODUKU

---
+++ Python 1_4/Recupero/Parte 1 -  Recupero/Parte 2 - Esercizi Impossibili/Esercizio - Soduku.py
@@ -0,0 +1,8 @@
+'''
+2) Scrivi un algoritmo che risolve sudoku
+
+l'algoritmo si chiama backtracking: https://it.wikipedia.org/wiki/Backtracking
+
+sudoku: https://it.wikipedia.org/wiki/Sudoku
+
+'''
