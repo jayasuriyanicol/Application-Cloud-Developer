53f2a1ad1195e8271670c5258947b8bd1e7c5c52
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2025-06-10 19:23:57 +0200

    Adding Only prompt of 3-SAT

--- Python 1_4/Recupero/Parte 1 -  Recupero/Parte 2 - Esercizi Impossibili/Esercizio delle 3-SAT.py
+++ Python 1_4/Recupero/Parte 1 -  Recupero/Parte 2 - Esercizi Impossibili/Esercizio delle 3-SAT.py
@@ -1,4 +1,9 @@
 '''
+3-SAT
+4) Scrivi un algoritmo che permette di capire se una formula 3-SAT è soddisfacibile.
 
+3-SAT: https://en.wikipedia.org/wiki/Boolean_satisfiability_problem
+
+Algoritmo DPLL:  https://en.wikipedia.org/wiki/DPLL_algorithm
 
 '''
