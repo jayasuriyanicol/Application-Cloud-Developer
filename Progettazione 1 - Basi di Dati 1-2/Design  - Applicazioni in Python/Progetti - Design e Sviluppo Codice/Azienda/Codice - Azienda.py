a640e936e6bddd84302f88edbf0ec07a6e1c9fb3
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2025-06-12 22:53:32 +0200

    UPDATE - 12_06_2025 | Code need some update

--- Progettazione 1 - Basi di Dati 1-2/Design  - Applicazioni in Python/Progetti - Design e Sviluppo Codice/Azienda/Codice - Azienda.py
+++ Progettazione 1 - Basi di Dati 1-2/Design  - Applicazioni in Python/Progetti - Design e Sviluppo Codice/Azienda/Codice - Azienda.py
@@ -10,14 +10,8 @@ Progetto -> valore attributo (nome:str, budget: Denaro)
 
 
 from __future__ import annotations
-from datetime import 
-
-
-
-def Denaro()-> None:
-
-    pass
-
+from datetime import date
+from typing import Any
 
 
 class Impiegato:
@@ -26,7 +20,7 @@ class Impiegato:
     _nome:str 
     _cognome:str
     _data_nascita:date    #-> <<immu>>
-    _stipendio : Denaro 
+    _stipendio : RealGEZ 
     _progetti : set()  #-> da assoc. 'Progetto'[ 0..*], certamnete non noto alla nasicta      
  
 
@@ -67,9 +61,39 @@ class Impiegato:
     def progetti(self)-> frozenset[partecipa]:
         return frozenset(self._progetti) 
     
+    
+    def _addLinkPartecipa(self,l:partecipa._link) -> None:
+         
+         if l.impiegato () != self:
+             
+             raise ValueError("Il link non coinvolge me!")
+         
+         if self.is_coinvolto(l.progetto()):
+            
+             raise ValueError("Il progetto era dell'Impiegato!")
+            
+         self._progetti.add(l)
+        
+         
+
+
+
+    def _removeLinkPartecipa(self,l:partecipa._link) -> None:
+        
+         if l.impiegato () != self:
+             
+             raise ValueError("Il link non coinvolge me!")
+         
+         if self.is_coinvolto(l.progetto()):
+            
+             raise ValueError("Il progetto era dell'Impiegato!")
+            
+         self._progetti.add(l)
+        
+
 
     def is_coinvolto(self, progetto:Progetto) -> bool:
-        
+
         for linkPartecipa in self._progetti ():
             if linkPartecipa.profetto() == progetto:
                 return True
@@ -81,40 +105,61 @@ class Impiegato:
 
 class partecipa:
 
-   #Ogni istanza rappresenta un link partecipa, ovvero coppia di (Progetto, Impiegato) con data  
+  @classmethod
+  def add(cls, progetto: Progetto, impiegato:Impiegato, data_inizio: date) -> _link:
+      
+       l = cls._link(progetto,impiegato,data_inizio)
+       progetto._addLinkPartecipa(l)
+       impiegato._addLinkPartecipa(l)
 
-    _data_inizio: date      #-> <<immu>>
-    _progetto: Progetto     #-> <<immu>> da assoc. 'partecipa'[ 0..*], certamnete non noto alla nasicta      
-    _impiegato : Impiegato  #-> <<immu>>  da assoc. 'partecipa'[ 0..*], certamnete non noto alla nasicta      
+       return l
+  
+  @classmethod
+  def remove(cls,l: _link) -> None:
+      
+      l.impiegato().removeLinkPartecipa(l)
+      l.progetto().removeLinkPartecipa(l)
+
+
+
+
+class _link:
+     #Ogni istanza rappresenta un link partecipa, ovvero coppia di (Progetto, Impiegato) con data  
+
+     _data_inizio: date      #-> <<immu>>
+     _progetto: Progetto     #-> <<immu>> da assoc. 'partecipa'[ 0..*], certamnete non noto alla nasicta      
+     _impiegato : Impiegato  #-> <<immu>>  da assoc. 'partecipa'[ 0..*], certamnete non noto alla nasicta      
   
 
 
-    def __init__(self, data_inizio:date)-> None:
+     def __init__(self, data_inizio:date)-> None:
 
         self._data_inizio = data_inizio
 
     
-    def impiegato(self) -> None:
+     def impiegato(self) -> None:
 
         return self._impiegato
     
-    def progetto(self) -> None:
+     def progetto(self) -> None:
 
         return self._progetto
     
 
 
-    def __hash__(self)-> int:
+     def __hash__(self)-> int:
 
         return hash ((self.progetto(), self.impiegato()))
     
-    def __eq__(self, other: Any ) -> bool:
+     def __eq__(self, other: Any ) -> bool:
 
         if type(other) != type(self) \
             or hash (self)!= type(self):
            return False
 
     
+
+
 
  
     
@@ -126,7 +171,7 @@ class Progetto:
 
     _nome : str
     _budget : float #-> che dobbiamo traformare in RealGEZ  
-    _impiegato : Impiegato #-> da assoc. 'partecipa'[ 0..*], certamnete non noto alla nasicta      
+    _impiegati : set[partecipa]  #-> da assoc. 'partecipa'[ 0..*], certamnete non noto alla nasicta      
  
 
 
@@ -137,7 +182,27 @@ class Progetto:
         self._budget = budget
 
         
+    def _addLinkPartecipa(self,l:partecipa._link) -> None:
+        
+         self._impiegati.add(l)
+    
 
+    def _removeLinkPartecipa(self,l:partecipa._link) -> None:
+        
+         self._impiegati.remove(l)
 
+    
 
+        
+
+    def impiegati(self,l: partecipa._link)-> frozenset[partecipa]:
+        return frozenset(self._impiegati) 
+    
+
+    def is_coinvolto(self, impiegato:Impiegato) -> bool:
+
+        for linkPartecipa in self._impiegati ():
+            if linkPartecipa.impiegato() == impiegato:
+                return True
+        return False
             
