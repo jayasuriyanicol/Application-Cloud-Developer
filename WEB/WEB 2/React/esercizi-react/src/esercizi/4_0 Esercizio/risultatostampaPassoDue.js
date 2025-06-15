155d27a6320893d68176495d9aea040f5c2a78ce
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2025-06-15 14:39:29 +0200

    UPDATE - 15_06_205 | Adding Fourth React Exercise

---
+++ WEB/WEB 2/React/esercizi-react/src/esercizi/4_0 Esercizio/risultatostampaPassoDue.js
@@ -0,0 +1,25 @@
+
+import React from "react";
+import StampaPassoDue from './esercizi/4_0 Esercizio/stampaPassoDue'
+
+/*4- Scrivere un programma che conta da 0 a 20 con passo 2 e stampa i numeri ottenuti (0,2,...,20) ;*/
+
+/*Per verificare il corretto funzionamento del programma copiarlo e incollarlo sul file 'App.js' e avviare l'ambiente React.JS */
+
+function App(){
+
+    return (
+
+        <div>
+        <h1>Numeri da 0 a 10 con passo 2 </h1>
+        <StampaPassoDue />
+        </div>
+
+    );
+
+
+}
+
+export default App;
+
+
