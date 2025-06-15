9c80bdfa4b29dd04e259aea8f585c8513a22ddfc
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2025-06-15 14:12:31 +0200

    UPDATE - 15_06_2025 | Adding Second ReactJS Exercise

---
+++ WEB/WEB 2/React/esercizi-react/src/esercizi/2_0 Esercizio/Tabellina.js
@@ -0,0 +1,45 @@
+import React from "react"
+{
+ /*2- Scrivere un componente Tabellina che stampi la tabellina di un numero, compreso
+tra 1 e 10 a vostro piacimento.*/
+
+ }
+
+
+ function Tabellina ({numeroTabellina = 5}){
+
+    const risultatoTabellina = []; 
+    
+
+    for (let i=1 ; i<=10; i++) {
+
+
+        risultatoTabellina.push(
+
+            <li key={i} className="list-group-item">
+               
+              {/*In questa maniera andaimo a creare una tabella con la sintassi: numero x numeroIterazione = numeroTabellina */}
+               {numeroTabellina} x {i} = {numeroTabellina * i}
+            </li>
+        );
+ }
+
+
+return (
+
+    <div className="container mt-4">
+        <div className="card shadow-sm">
+            <div className="card-body">
+
+                <h5 className="card-title">Tabellina del numero {numeroTabellina}</h5>
+                <ul className="list-group">{risultatoTabellina}</ul>
+            </div>
+        </div>
+    </div>
+
+
+
+);
+}
+
+export default Tabellina;
---
+++ WEB/WEB 2/React/esercizi-react/src/esercizi/2_0 Esercizio/risultatoTabellina.js
@@ -0,0 +1,27 @@
+{/*
+
+2- Scrivere un componente Tabellina che stampi la tabellina di un numero, compreso
+tra 1 e 10 a vostro piacimento.
+
+*/ }
+
+{/*Per verificare il corretto funzionamento del programma copiarlo e incollarlo sul file 'App.js' e avviare l'ambiente React.JS */}
+
+
+
+import React from "react";
+import Tabellina from "./esercizi/2_0 Eserczio/Tabellina";
+
+function App (){
+
+    return (
+
+        <div>
+             <h1>Esercizio Tabellina</h1>
+            <Tabellina />
+
+        </div>
+    );
+}
+
+export default App;
