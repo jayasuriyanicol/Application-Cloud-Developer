27c96e3ec7ad1067e0182969f3e4fe021632d282
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2025-06-15 12:50:59 +0200

    UPDATE - 15_06_205 | Adding FIrst React Exercise

---
+++ WEB/WEB 2/React/esercizi-react/src/esercizi/1_0 Esercizio/risultatoPersona.js
@@ -0,0 +1,20 @@
+{/*
+1- Scrivere un componente Persona che mostri a video i dati anagrafici contenuti in
+un oggetto persona. Formattare il layout con bootstrap
+*/ }
+
+{/*Per verificare il corretto funzionamento del programma copiarlo e incollarlo sul file 'App.js' e avviare l'ambiente React.JS */}
+
+import React from 'react';
+import Persona from './esercizi/1_0 Esercizio/Persona';
+
+function App() {
+  return (
+    <div>
+      <h1>Esercizi React</h1>
+      <Persona />
+    </div>
+  );
+}
+
+export default App;
