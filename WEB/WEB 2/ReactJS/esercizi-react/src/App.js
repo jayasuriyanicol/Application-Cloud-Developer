8ba9978c27ab9e9b9a5a52b9aec6d405bc607f99
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2025-06-17 12:00:15 +0200

    UPDATE - 17_06_205 |  Return the previous solution

--- WEB/WEB 2/ReactJS/esercizi-react/src/App.js
+++ WEB/WEB 2/ReactJS/esercizi-react/src/App.js
@@ -1,12 +1,46 @@
-import React from "react";
-import Biblioteca from "./esercizi/5_0 Esercizio/Biblioteca";
+
+import React, { useState } from 'react';
+import Componente1 from './Componente1';
+import Clock from './Clock';
+
+function getDate(date) {
+  return date.toLocaleDateString() + " " + date.toLocaleTimeString();
+}
+
+let nome = "Nicol";
 
 function App() {
+  const [messaggio, setMessaggio] = useState("");
+  const now = new Date();
+
+  const saluta = () => {
+    setMessaggio("Ciao " + nome + "!");
+  };
+
   return (
-    <div className="App">
-      <Biblioteca />
+    <div className="App container text-center mt-5">
+      <Componente1 nome="Nicol" cognome="Jayasuriya" eta="21" />
+      <Componente1 nome="Nathan" cognome="Mbuyamba" eta="21" />
+      <Componente1 nome="Michael" cognome="Baciarello" eta="21" />
+
+      <h1>Prima App React Benvenuto {nome} !<br></br></h1>
+
+      <h2>{getDate(now)}</h2>
+
+      <Clock timezone="0" country="Italia" />
+      <Clock timezone="-6" country="USA" />
+      <Clock timezone="7" country="Japan" />
+
+      <button className="btn btn-primary mt-3" onClick={saluta}>
+        Salutami
+      </button>
+
+      {/* Messaggio dinamico */}
+      {messaggio && <p className="mt-3">{messaggio}</p>}
     </div>
   );
 }
 
 export default App;
+
+
