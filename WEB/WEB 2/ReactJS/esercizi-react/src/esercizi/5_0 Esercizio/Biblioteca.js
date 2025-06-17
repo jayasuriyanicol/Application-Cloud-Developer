b1f789e032df9350ae785284babcaee5d74f5659
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2025-06-17 11:46:37 +0200

    UPDATE - 17_06_205 | Adding Re-load process on Library Exercise

--- WEB/WEB 2/ReactJS/esercizi-react/src/esercizi/5_0 Esercizio/Biblioteca.js
+++ WEB/WEB 2/ReactJS/esercizi-react/src/esercizi/5_0 Esercizio/Biblioteca.js
@@ -6,24 +6,37 @@
 2.Componente BookList: Un componente che mostra la lista dei libri.
 
 3. Componente AddBookForm: Un componente che al momento mostra solo la scritta ‘form inserimento libro’ 
-con un pulsante che al click mostra un alert(‘libro inserito’) */
+con un pulsante che al click mostra un alert(‘libro inserito’) 
 
+*/
 
-import React, { useState } from "react";
+import React from "react";
 import BookList from "./BookList";
 import AddBookForm from "./addBookForm";
 
-function Biblioteca() {
-  const [libri, setLibri] = useState([
+let libri = [
+
     { titolo: "Il mio segreto - J.B.Cooper" },
     { titolo: "La storia dei 4 ragazzi" },
     { titolo: "Il mio nome è Diego !" }
-  ]);
 
-  const aggiungiLibro = (titolo) => {
-    setLibri([...libri, { titolo }]);
-  };
+];
 
+function aggiungiLibro(titolo) {
+  libri.push({ titolo });
+
+  //Aggiungiamo la lista dei libri già esistenti nel DOM, creando prima la colonna e poi la riga per il nuovo libro passando come parametro il libro
+  const ul = document.getElementById("lista-libri");
+  const li = document.createElement("li");
+  li.className = "list-group-item";
+  li.textContent = titolo;
+  ul.appendChild(li);
+
+  alert("Libro inserito!");
+}
+
+
+function Biblioteca() {
   return (
     <div className="container mt-4">
       <h2>Biblioteca</h2>
