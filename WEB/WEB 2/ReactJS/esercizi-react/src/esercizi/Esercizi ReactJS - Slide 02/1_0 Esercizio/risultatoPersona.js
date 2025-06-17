4cad7f5b3e449e36dbc4ccc60dacf02d9ed80c26
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2025-06-17 11:59:29 +0200

    UPDATE - 17_06_205 |  Adding Correct RElative Path for each Exercise

--- WEB/WEB 2/ReactJS/esercizi-react/src/esercizi/Esercizi ReactJS - Slide 02/1_0 Esercizio/risultatoPersona.js
+++ WEB/WEB 2/ReactJS/esercizi-react/src/esercizi/Esercizi ReactJS - Slide 02/1_0 Esercizio/risultatoPersona.js
@@ -1,12 +1,13 @@
+
+import React from 'react';
+import Persona from './esercizi/Esercizi ReactJS - Slide 02/1_0 Esercizio/Persona';
+
 {/*
 1- Scrivere un componente Persona che mostri a video i dati anagrafici contenuti in
 un oggetto persona. Formattare il layout con bootstrap
 */ }
 
 {/*Per verificare il corretto funzionamento del programma copiarlo e incollarlo sul file 'App.js' e avviare l'ambiente React.JS */}
-
-import React from 'react';
-import Persona from './esercizi/1_0 Esercizio/Persona';
 
 function App() {
   return (
