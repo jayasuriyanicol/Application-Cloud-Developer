7ccfdd2a7c3f265681679babc54c365162fddb96
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2025-06-17 11:46:41 +0200

    UPDATE - 17_06_205 | Adding Re-load process on Library Exercise

--- WEB/WEB 2/ReactJS/esercizi-react/src/esercizi/5_0 Esercizio/BookList.js
+++ WEB/WEB 2/ReactJS/esercizi-react/src/esercizi/5_0 Esercizio/BookList.js
@@ -2,16 +2,16 @@ import React from "react";
 
 function BookList({ libri }) {
   return (
-    <div className="mt-3">
-      <h4>Lista dei Libri</h4>
-      <ul className="list-group">
-        {libri.map((libro, index) => (
-          <li key={index} className="list-group-item">
-            {index + 1}) {libro.titolo}
-          </li>
-        ))}
-      </ul>
-    </div>
+    <ul className="list-group" id="lista-libri">
+
+      {libri.map((libro, index) => (
+
+        <li key={index} className="list-group-item">
+          
+          {libro.titolo}
+        </li>
+      ))}
+    </ul>
   );
 }
 
