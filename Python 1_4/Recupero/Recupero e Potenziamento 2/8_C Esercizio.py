30e921894390b78192eae93f57c0c8188b72f1a4
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2025-06-17 13:01:09 +0200

    UPDATE - 17_06_2025 |  Completed Solution

--- Python 1_4/Recupero/Recupero e Potenziamento 2/8_C Esercizio.py
+++ Python 1_4/Recupero/Recupero e Potenziamento 2/8_C Esercizio.py
@@ -25,30 +25,30 @@ from typing import Any
 
 class Frazione:
 
-    ___numeratore: int|float
-    ___denominatore: int|float
+    ___numeratore: int|float|Any
+    ___denominatore: int|float|Any
 
-    def __init__(self,numeratore:int|float, denominatore:int|float) -> None:
+    def __init__(self,numeratore:int|float|Any, denominatore:int|float|Any) -> None:
 
         self.___numeratore = numeratore
         self.___denominatore = denominatore
     
     def __repr__(self):
         
-        return f"{self.___numeratore} \ {self.___denominatore}"
+        return f"{self.___numeratore} / {self.___denominatore}"
     
-    def MassimoComuneDivisore(self, x:int|float, Y:int|float) -> int|float:
+    def MassimoComuneDivisore(self, x:int|float|Any, y:int|float|Any) -> int|float|Any:
 
-        x:int|float = abs(self.___numeratore)
-        y:int|float = abs(self.___denominatore)
+        x:int|float|Any = abs(x)
+        y:int|float|Any = abs(y)
         valoreMinimo:int = min(x,y)
 
-        for elementoFrazione in (valoreMinimo, 0, -1):
+        for elementoFrazione in range (valoreMinimo, 0, -1):
 
             if x % elementoFrazione == 0 and y % elementoFrazione == 0:
 
                 return elementoFrazione
-            return 1
+        return 1
     
 
     def semplifica(self):
@@ -57,21 +57,11 @@ class Frazione:
         valoreMcD:int = self.MassimoComuneDivisore(self.___numeratore,self.___denominatore )
 
         return Frazione(self.___numeratore // valoreMcD , self.___denominatore // valoreMcD)
-
-
-
-
-
     
-    
-        
-
-
- 
 
         
 
-def semplifica(listaFrazioni:Frazione) -> list[Frazione]:
+def semplifica(listaFrazioni:list[Frazione] ) -> list[Frazione]:
 
 
     listaFrazioneIrriducibili:list[Frazione] = [] 
@@ -79,7 +69,23 @@ def semplifica(listaFrazioni:Frazione) -> list[Frazione]:
 
     for frazione in listaFrazioni:
 
-        if frazione
+        frazioneNonSemplificata = frazione.semplifica()
+        listaFrazioneIrriducibili.append(frazioneNonSemplificata)
+    
+    return listaFrazioneIrriducibili
 
 
 
+
+
+
+'''DRIVER PROGRAMM - Notiamo la bontà della soluzione'''
+
+if __name__ == "__main__":
+
+
+    listaFrazione = [Frazione(3,5), Frazione(8,4), Frazione(3,7), Frazione(18,6) ] 
+    
+    risultatoFrazioneIrriducibile = semplifica(listaFrazione)
+
+    print(risultatoFrazioneIrriducibile)
