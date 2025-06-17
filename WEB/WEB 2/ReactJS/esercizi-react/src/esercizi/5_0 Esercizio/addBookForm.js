d2a6592d0fb68b792c80aca3c1292077e6a84519
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2025-06-17 11:46:34 +0200

    UPDATE - 17_06_205 | Adding Re-load process on Library Exercise

--- WEB/WEB 2/ReactJS/esercizi-react/src/esercizi/5_0 Esercizio/addBookForm.js
+++ WEB/WEB 2/ReactJS/esercizi-react/src/esercizi/5_0 Esercizio/addBookForm.js
@@ -1,33 +1,22 @@
-import React, { useState } from "react";
+import React from "react";
 
 function AddBookForm({ onAggiungiLibro }) {
-  const [titolo, setTitolo] = useState("");
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (titolo.trim()) {
+  function handleClick() {
+
+    const titolo = prompt("Inserisci il titolo del libro:");
+    
+    if (titolo) {
       onAggiungiLibro(titolo);
-      alert("Libro inserito");
-      setTitolo("");
     }
-  };
+  }
 
   return (
-    <form onSubmit={handleSubmit} className="mt-3">
-      <h4>Form inserimento libro</h4>
-      <div className="input-group">
-        <input
-          type="text"
-          value={titolo}
-          onChange={(e) => setTitolo(e.target.value)}
-          className="form-control"
-          placeholder="Titolo del libro"
-        />
-        <button type="submit" className="btn btn-primary">
-          Aggiungi
-        </button>
-      </div>
-    </form>
+    <div className="mt-3">
+      <button className="btn btn-success" onClick={handleClick}>
+        Aggiungi libro
+      </button>
+    </div>
   );
 }
 
