aefba00053bbb027ee12ca2096bd69e3438691fe
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2025-06-20 13:24:25 +0200

    UPDATE - 20_06_2025 | Adding New Exercise not completed (only hint)

---
+++ Python 1_4/Recupero/Recupero e Potenziamento 2/Esercizio 1/8_E Esercizio.py
@@ -0,0 +1,9 @@
+'''
+8.E Scrivere un codice Python che inizializzi la seguente lista l di frazioni, dove ogni frazione è un oggetto della classe Frazione:
+ 
+l = 2.5/0,   1/2,   2/4,   3/5,   6/9,   4/7,   24/36,   12/36,   40/60,   5/11,   10/45,   42/78,   9/15
+       
+Sfruttando la funzione semplifica, generare una nuova lista chiamata l_s, contente una versione semplificata delle frazioni della lista l.
+Infine, richiamando la funzione fractionCompare, dimostrare che le funzioni delle lista l e l_s sono equivalenti, ovvero hanno lo stesso valore.
+
+'''
