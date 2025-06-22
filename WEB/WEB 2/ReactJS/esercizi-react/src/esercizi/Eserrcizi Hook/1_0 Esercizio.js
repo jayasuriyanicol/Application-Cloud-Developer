af2dbba53c8c402fd9fb271466cc452b2d937203
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2025-06-22 16:16:23 +0200

    UPDATE - 22_06_2025 |Adding New JavaScript Hook Exercise

--- WEB/WEB 2/ReactJS/esercizi-react/src/esercizi/Eserrcizi Hook/1_0 Esercizio.js
+++ WEB/WEB 2/ReactJS/esercizi-react/src/esercizi/Eserrcizi Hook/1_0 Esercizio.js
@@ -1,4 +1,13 @@
-/*ESERCIZIO:
+/*ESERCIZIO numero 1:
 
 Creare un componente che rappresenta per la mia applicazione un  contatore utilizzando useState con un valore di default uguale a zero.
-Sotto il contatore andiamo a creare due bottoni*/
+Sotto il contatore andiamo a creare due bottoni, uno <<diminuisci>> ed uno <<aumenta>> che al loro click andranno ad aumentare e a 
+diminuire il valore del contatore.
+
+
+Utilizzando bootstrap per rendere gradevole il nostro componente (a nostro piacimento)
+
+*/
+
+import React from "react"
+
