ba9c96d516ae5d188b50d4613419f406d4dfadbf
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2025-06-26 17:43:11 +0200

    UPDATE - 26_06_2025 | Adding New Exercise

---
+++ Python 1_4/Recupero/Recupero e Potenziamento 5/Esercizio numero 1/1_0 Esercizio - Consegna.py
@@ -0,0 +1,24 @@
+'''
+CONSEGNA ESERCIZIO:
+
+Esercizio 1.
+ 
+Data una matrice quadrata di dimensione m x m, il carico di una posizione (r,c), indicato con k(r,c),
+è dato dalla differenza tra la somma degli elementi della riga r e la somma degli elementi della colonna c.
+Ad esempio, data la seguente matrice:
+ 
+1   2   1   1
+0   0   0   1
+1   1   2   0
+2   0   0   0
+ 
+alcuni dei carichi sono:
+ 
+
+c(0,0) = 1
+
+c(1,0) = -3
+
+c(3,3) = 0
+
+'''
