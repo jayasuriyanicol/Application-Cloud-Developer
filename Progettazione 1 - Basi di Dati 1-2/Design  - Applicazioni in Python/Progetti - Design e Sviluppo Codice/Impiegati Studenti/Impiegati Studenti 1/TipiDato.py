5e25764a3316be52e086027e7f36119bd714c864
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2025-06-29 10:55:27 +0200

    UPDATE - 29_06_2025 | Adding  "Address" on new types of data

--- Progettazione 1 - Basi di Dati 1-2/Design  - Applicazioni in Python/Progetti - Design e Sviluppo Codice/Impiegati Studenti/Impiegati Studenti 1/TipiDato.py
+++ Progettazione 1 - Basi di Dati 1-2/Design  - Applicazioni in Python/Progetti - Design e Sviluppo Codice/Impiegati Studenti/Impiegati Studenti 1/TipiDato.py
@@ -88,3 +88,42 @@ class CAP(str):
             return super().__new__(cls, v)
         raise ValueError(f"'{v}' non è un CAP italiano valido!")
 
+
+
+class Indirizzo:
+    # campi dati:
+    _via:str
+    _civico: str
+    _cap: CAP
+    def __init__(self, via: str, civico: str, cap: CAP) -> None:
+        self._via: str = via
+
+        if not re.search("^[0-9]+[a-zA-Z]*$", civico):
+            raise ValueError(f"value for civico '{civico}' not allowed")
+        self._civico: str = civico
+        self._cap: CAP = cap
+
+    def via(self) -> str:
+        return self._via
+
+    def civico(self) -> str:
+        return self._civico
+
+    def cap(self) -> str:
+        return self._cap
+
+    def __repr__(self) -> str:
+        return f"Indirizzo(via={self.via()}, civico={self.civico()}, cap={self.cap()})"
+
+    def __str__(self) -> str:
+        return f"{self.via()} {self.civico()} - {self.cap()}"
+
+    # class Indirizzo implementa un tipo di dato: Python deve riconoscere se oggetti diversi rappresentano lo stesso valore
+    def __hash__(self) -> int:
+        return hash((self.via(), self.civico(), self.cap()))
+
+    def __eq__(self, other: Any) -> bool:
+        if not isinstance(other, type(self)) or \
+                hash(self) != hash(other):
+            return False
+        return (self.via(), self.civico(), self.cap()) == (other.via(), other.civico(), other.cap())
