84e29bb92cff4fb45a047dd81e7cd220444736be
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2025-06-30 12:00:38 +0200

    UPDATE - 30_06_2025  | Adding new useEffect component "FatchComponents"

---
+++ WEB/WEB 2/ReactJS/esercizi-react/src/esercizi/UseEffect/FetchComponents.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react'
+
+const url="https://jsonplaceholder.typicode.com/photos";
+const FetchComponents = () => {
+
+    const[photos,setPhotos] = useState([]);
+    const getData=Asynvc () =>{
+
+        const photos= await fetch(url).then(ris=>ris.json())
+        setPhotos(getData)
+    }
+
+    useEffect(()=>{
+
+        getData(); 
+   },[])
+
+
+  return (
+    <>
+    <h1>FetchComponents</h1>
+    <ul className="users">
+
+       {photos.map((el) =>{
+
+        const {title, id, thumbnaiUrl: img, url} = el;
+        return (
+
+
+            <li key={id} className='shadow'>
+                <img src={img} alt ={title} />
+                <div>
+                    <h5>{id} - {title}</h5>
+                    <a href={url}>Profile</a>
+                </div>
+            </li>
+        )
+    })}
+</ul>
+</>
+
+export default FetchComponents
