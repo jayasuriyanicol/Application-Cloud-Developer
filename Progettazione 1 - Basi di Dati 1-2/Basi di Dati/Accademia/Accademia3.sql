4eca60e17623a07ccb36af05fdfabb79eb7c6275
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2025-07-07 18:19:03 +0200

    UPDATE - 07_07_2025 | Adding New Folder Accademia and new exercise Accademia3

--- Progettazione 1 - Basi di Dati 1-2/Basi di Dati/Accademia/Accademia3.sql
+++ Progettazione 1 - Basi di Dati 1-2/Basi di Dati/Accademia/Accademia3.sql
@@ -2,13 +2,29 @@
 
 --CREAZIONE DELL'INSIEME DEI Domini:
 
-CREATE TYPE Strutturato as ENUM ("Ricercatore","Professore Associato","Professore Ordinario");
+CREATE TYPE Strutturato as ENUM ('Ricercatore','Professore Associato','Professore Ordinario');
 
 CREATE TYPE LavoroProgetto as ENUM ('Ricerca e Sviluppo', 'Dimostrazione', 'Management', 'Altro');
 
-CREATE TYPE LavoroNonProgettuale as ENUM ('Didattica','Ricerca','Missione','Incontro Dipartimentale', 'Incontro Accademico')
+CREATE TYPE LavoroNonProgettuale as ENUM ('Didattica','Ricerca','Missione','Incontro Dipartimentale', 'Incontro Accademico');
 
 CREATE TYPE CausaAssenza as ENUM ('Chiusura Universitaria', 'Maternita', 'Malattia');
 
 CREATE DOMAIN PosInteger as INTEGER:
    CHECK (value > 0);
+
+CREATE DOMAIN StringaM as VARCHAR(100);
+
+CREATE DOMAIN NumeroOre as INTEGER:
+    CHECK (0 => value <= 8);
+
+
+
+
+CREATE TABLE Persona (
+    
+nome VARCHAR (100) NOT NULL,
+cognome VARCHAR (100) NOT NULL,
+posizione Strutturato
+stipendio Denaro 
+)
