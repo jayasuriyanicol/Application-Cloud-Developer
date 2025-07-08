6389fcdb6ccd2e4f58b0cc5549b95d733a26fad0
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2025-07-08 10:52:45 +0200

    UPDATE - 07_07_2025 | Adding INSERTION DB for table "Persona" and "Progetto"

--- Progettazione 1 - Basi di Dati 1-2/Basi di Dati/Accademia/Accademia3.sql
+++ Progettazione 1 - Basi di Dati 1-2/Basi di Dati/Accademia/Accademia3.sql
@@ -1,78 +1,64 @@
--- CREAZIONE DEL DataBase ACCADEMIA 3
+-- CREAZIONE DEL DATABASE ACCADEMIA 3
 CREATE DATABASE accademia3;
 \c accademia3
 
---CREAZIONE DELL'INSIEME DEI Domini:
+-- CREAZIONE DEI TIPI ENUM E DOMINI
 
-CREATE TYPE Strutturato as ENUM ('Ricercatore','Professore Associato','Professore Ordinario');
+CREATE TYPE Strutturato AS ENUM ('Ricercatore', 'Professore Associato', 'Professore Ordinario');
 
-CREATE TYPE LavoroProgetto as ENUM ('Ricerca e Sviluppo', 'Dimostrazione', 'Management', 'Altro');
+CREATE TYPE LavoroProgetto AS ENUM ('Ricerca e Sviluppo', 'Dimostrazione', 'Management', 'Altro');
 
-CREATE TYPE LavoroNonProgettuale as ENUM ('Didattica','Ricerca','Missione','Incontro Dipartimentale', 'Incontro Accademico');
+CREATE TYPE LavoroNonProgettuale AS ENUM ('Didattica', 'Ricerca', 'Missione', 'Incontro Dipartimentale', 'Incontro Accademico');
 
-CREATE TYPE CausaAssenza as ENUM ('Chiusura Universitaria', 'Maternita', 'Malattia');
+CREATE TYPE CausaAssenza AS ENUM ('Chiusura Universitaria', 'Maternita', 'Malattia');
 
-CREATE DOMAIN PosInteger as INTEGER
-   CHECK (value > 0);
+CREATE DOMAIN PosInteger AS INTEGER
+    CHECK (VALUE > 0);
 
-CREATE DOMAIN StringaM as VARCHAR(100);
+CREATE DOMAIN StringaM AS VARCHAR(100);
 
-CREATE DOMAIN NumeroOre as INTEGER
-    CHECK (0 => value <= 8);
+CREATE DOMAIN NumeroOre AS INTEGER
+    CHECK (VALUE >= 0 AND VALUE <= 8);
 
-CREATE DOMAIN Denaro as REAL
-    CHECK (value >= 0);
+CREATE DOMAIN Denaro AS REAL
+    CHECK (VALUE >= 0);
 
 
-
--- CREAZIONE DELLE TABELLE 
-
+-- CREAZIONE DELLE TABELLE
 
 CREATE TABLE Persona (
-    
     id PosInteger NOT NULL,
     nome StringaM NOT NULL,
-    cognome StringaM  NOT NULL,
+    cognome StringaM NOT NULL,
     posizione Strutturato NOT NULL,
     stipendio Denaro NOT NULL,
-
     PRIMARY KEY (id)
-
 );
 
 
 CREATE TABLE Progetto (
-
     id PosInteger NOT NULL,
     nome StringaM NOT NULL,
     inizio DATE NOT NULL,
     fine DATE NOT NULL,
     budget Denaro NOT NULL,
-    
-
     PRIMARY KEY (id),
     CHECK (inizio < fine),
-    UNIQUE(nome)
-
-
+    UNIQUE (nome)
 );
 
 
-
 CREATE TABLE WP (
-
     progetto PosInteger NOT NULL,
     id PosInteger NOT NULL,
     nome StringaM NOT NULL,
     inizio DATE NOT NULL,
     fine DATE NOT NULL,
-    
+    PRIMARY KEY (progetto, id),
     CHECK (inizio < fine),
-    FOREIGN KEY progetto REFERENCES Progetto(id),
-    UNIQUE(progetto,nome)
+    FOREIGN KEY (progetto) REFERENCES Progetto(id),
+    UNIQUE (progetto, nome)
 );
-
-
 
 
 CREATE TABLE AttivitaProgetto (
@@ -83,45 +69,39 @@ CREATE TABLE AttivitaProgetto (
     giorno DATE NOT NULL,
     tipo LavoroProgetto NOT NULL,
     oreDurata NumeroOre NOT NULL,
-    
     PRIMARY KEY (id),
-    FOREIGN KEY persona REFERENCES Persona(id),
-    FOREIGN KEY (progetto,wp) REFERENCES WP(progetto,id)
+    FOREIGN KEY (persona) REFERENCES Persona(id),
+    FOREIGN KEY (progetto, wp) REFERENCES WP(progetto, id)
 );
 
 
-
-
 CREATE TABLE AttivitaNonProgettuale (
-
     id PosInteger NOT NULL,
     persona PosInteger NOT NULL,
     tipo LavoroNonProgettuale NOT NULL,
     wp PosInteger NOT NULL,
     giorno DATE NOT NULL,
-    oreDurata NumeroOre NOT NULL,  
-
-    PRIMARY KEY (id),  
-    FOREIGN KEY persona REFERENCES Persona(id),
+    oreDurata NumeroOre NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (persona) REFERENCES Persona(id)
     
 );
 
 
 CREATE TABLE Assenza (
-
     id PosInteger NOT NULL,
     persona PosInteger NOT NULL,
     tipo CausaAssenza NOT NULL,
     giorno DATE NOT NULL,
-    oreDurata NumeroOre NOT NULL,  
-
-    PRIMARY KEY (id),  
-    FOREIGN KEY persona REFERENCES Persona(id),
-    UNIQUE (persona,giorno)
+    oreDurata NumeroOre NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (persona) REFERENCES Persona(id),
+    UNIQUE (persona, giorno)
 );
 
 
-
+INSERT INTO persona VALUES (1,'Nicol','Jayasuriya','Professore Associato', 3100.45);
+INSERT INTO progetto VALUES (1, 'ReenbacRailways', TO_DATE ('22-05-2025', 'DD-MM-YYYY'), TO_DATE ('24-11-2030', 'DD-MM-YYYY'), 3000000.00);
 
 
 
