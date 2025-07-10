c8008b4f33889333e3505f40c150b4d902f4361b
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2025-07-10 18:19:09 +0200

    UPDATE - 10_07_2025 | Improving Stuff of the Code

--- WEB/WEB 2/ReactJS/esercizi-react/src/esercizi/UseEffect/UseEffect.js
+++ WEB/WEB 2/ReactJS/esercizi-react/src/esercizi/UseEffect/UseEffect.js
@@ -1,44 +1,37 @@
-import React,{ useEffect, useState} from "react";
-
+import React, { useEffect, useState } from "react";
 
 const EsempioUseEffect = () => {
-    
+    const [valore, setValore] = useState(0);
 
-    const[valore,setValore] =useState(0); 
-    
-    const funzio=()=>{
-        console.log("Funziona")
+    const funzio = () => {
+        console.log("Funziona");
     };
+
+    useEffect(funzio, []);
+
     useEffect(() => {
-
-        if(valore < 1){
-             document.title = "Nessun valore"
-        }else{
-            document.title = "C'è qualcosa ..."
+        if (valore < 1) {
+            document.title = "Nessun valore";
+        } else {
+            document.title = "C'è qualcosa ...";
         }
-         console.log("Chiamato useEffect");
+        console.log("Chiamato useEffect");
 
-         return (() => (console.log("Chiamata funzione CleanUp !")))
-    
-
-   },[valore]);
-}
-   /*
-   Lo useEffect viene vincolato,ovvero con :
-   1.[] eseuguo una sola volta
-   2. Senza paramtro eseguo sempre
-   3. Con parametri eseguo vincolato al cambio di stato dei parametri passati 1-n 
-   */
-
-   useEffect(funzio,[]);
+        return () => {
+            console.log("Chiamata funzione CleanUp !");
+        };
+    }, [valore]);
 
     console.log("Fuori dallo useEffect");
 
-   return (
-    <>
-    <div>Esempio useEffect</div>
-    <button className="btn btn-secondarynpm" onClick={()=>setValore(valore>valore+1)}>Aumenta</button> 
-    </>
-   );
+    return (
+        <>
+            <div>Esempio useEffect</div>
+            <button className="btn btn-secondary" onClick={() => setValore(valore + 1)}>
+                Aumenta
+            </button>
+        </>
+    );
+};
 
 export default EsempioUseEffect;
