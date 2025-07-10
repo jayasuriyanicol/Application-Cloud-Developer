f7b604c1d04eb392fee27c5ecbed81cfffdf0a29
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2025-07-10 18:18:25 +0200

    UPDATE - 10_07_2025 | Improving Stuff of the Code

--- WEB/WEB 2/ReactJS/esercizi-react/src/esercizi/UseEffect/FetchComponents.js
+++ WEB/WEB 2/ReactJS/esercizi-react/src/esercizi/UseEffect/FetchComponents.js
@@ -1,43 +1,38 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 
-const url="https://jsonplaceholder.typicode.com/photos";
+const url = "https://jsonplaceholder.typicode.com/photos";
+
 const FetchComponents = () => {
+    const [photos, setPhotos] = useState([]);
 
-    const[photos,setPhotos] = useState([]);
-    
-    const getData= async function () {
+    const getData = async () => {
+        const data = await fetch(url).then(res => res.json());
+        setPhotos(data);
+    };
 
-        const photos= await fetch(url).then(ris=>ris.json())
-        setPhotos(getData)
-    }
+    useEffect(() => {
+        getData();
+    }, []);
 
-    useEffect(()=>{
-
-        getData(); 
-   },[])
-
-
-  return (
-    <>
-    <h1>FetchComponents</h1>
-    <ul className="users">
-
-       {photos.map((el) =>{
-
-        const {title, id, thumbnaiUrl: img, url} = el;
-        return (
-
-
-            <li key={id} className='shadow'>
-                <img src={img} alt ={title} />
-                <div>
-                    <h5>{id} - {title}</h5>
-                    <a href={url}>Profile</a>
-                </div>
-            </li>
-        )
-    })}
-</ul>
-</>
+    return (
+        <>
+            <h1>FetchComponents</h1>
+            <ul className="users">
+                {photos.slice(0, 10).map((el) => {
+                    const { title, id, thumbnailUrl: img, url } = el;
+                    return (
+                        <li key={id} className="shadow">
+                            <img src={img} alt={title} />
+                            <div>
+                                <h5>{id} - {title}</h5>
+                                <a href={url}>Profile</a>
+                            </div>
+                        </li>
+                    );
+                })}
+            </ul>
+        </>
+    );
+};
 
 export default FetchComponents;
