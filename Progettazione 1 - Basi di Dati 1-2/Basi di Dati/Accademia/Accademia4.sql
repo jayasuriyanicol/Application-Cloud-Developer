f9913d152e69f2355a6551cdef23ad23b472f1e8
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2025-07-12 16:03:52 +0200

    UPDATE - 12_07_2025 |  Reording id in according to the  check value

--- Progettazione 1 - Basi di Dati 1-2/Basi di Dati/Accademia/Accademia4.sql
+++ Progettazione 1 - Basi di Dati 1-2/Basi di Dati/Accademia/Accademia4.sql
@@ -99,56 +99,64 @@ CREATE TABLE Assenza (
     UNIQUE (persona, giorno)
 );
 
+
+-- POPOLAMENTO DELLE TABELLE
+
 INSERT INTO Persona VALUES
-(0, 'Anna', 'Bianchi', 'Ricercatore', 30000),
-(1, 'Mario', 'Rossi', 'Ricercatore', 32000),
-(2, 'Barbara', 'Burso', 'Ricercatore', 31000),
-(3, 'Gino', 'Spada', 'Ricercatore', 30500),
-(7, 'Andrea', 'Verona', 'Professore Associato', 45000),
-(12, 'Dario', 'Basile', 'Ricercatore', 33000),
-(15, 'Leonardo', 'Vitali', 'Professore Ordinario', 50000),
-(16, 'Paolo', 'Valentini', 'Professore Associato', 47000),
-(18, 'Giulia', 'Costa', 'Ricercatore', 31500),
-(20, 'Carla', 'Martinelli', 'Ricercatore', 30000);
+(1, 'Anna', 'Bianchi', 'Ricercatore', 30000),
+(2, 'Mario', 'Rossi', 'Ricercatore', 32000),
+(3, 'Barbara', 'Burso', 'Ricercatore', 31000),
+(4, 'Gino', 'Spada', 'Ricercatore', 30500),
+(5, 'Andrea', 'Verona', 'Professore Associato', 45000),
+(6, 'Dario', 'Basile', 'Ricercatore', 33000),
+(7, 'Leonardo', 'Vitali', 'Professore Ordinario', 50000),
+(8, 'Paolo', 'Valentini', 'Professore Associato', 47000),
+(9, 'Giulia', 'Costa', 'Ricercatore', 31500),
+(10, 'Carla', 'Martinelli', 'Ricercatore', 30000);
 
 
 INSERT INTO Progetto VALUES
-(0, 'Artemide', '2000-01-01', '2002-12-31', 255000),
-(1, 'Pegasus', '2012-01-01', '2014-12-31', 330000),
-(2, 'WineSharing', '1999-01-01', '2003-12-31', 998000),
-(3, 'Simap', '2010-02-01', '2014-03-17', 158000),
-(4, 'DropDiscovery', '2010-09-13', '2013-01-20', 99000);
+(1, 'Artemide', '2000-01-01', '2002-12-31', 255000),
+(2, 'Pegasus', '2012-01-01', '2014-12-31', 330000),
+(3, 'WineSharing', '1999-01-01', '2003-12-31', 998000),
+(4, 'Simap', '2010-02-01', '2014-03-17', 158000),
+(5, 'DropDiscovery', '2010-09-13', '2013-01-20', 99000);
+
 
 
 INSERT INTO WP VALUES
-(2, 2, 'Dissemination', '2000-02-01', '2002-01-01'),
-(3, 1, 'Main Activity', '2010-02-01', '2012-01-01'),
-(0, 0, 'State of the Art', '1999-01-01', '2000-01-01'),
-(1, 1, 'Main Research', '2010-03-01', '2011-12-01'),
-(0, 0, 'WP1', '1999-01-01', '2000-06-01'),
-(1, 1, 'WP2', '2010-06-01', '2011-06-01'),
-(2, 2, 'WP3', '2000-01-01', '2002-01-01');
+(3, 1, 'Dissemination', '2000-02-01', '2002-01-01'),
+(4, 1, 'WP3', '2000-05-01', '2002-01-01'),
+(4, 2, 'Main Activity', '2010-02-01', '2012-01-01'),
+(4, 3, 'WP2', '2012-02-01', '2013-01-01'),
+(1, 1, 'WP1', '1999-02-01', '2000-01-01'),
+(2, 1, 'Main Research', '2010-03-01', '2011-12-01'),
+(1, 2, 'State of the Art', '1999-01-01', '2000-01-01');
 
 
 
 INSERT INTO AttivitaProgetto VALUES
-(0, 0, 2, 2, '2001-03-01', 'Ricerca e Sviluppo', 6),
-(1, 1, 2, 2, '2001-04-01', 'Management', 4),
-(2, 2, 2, 2, '2001-05-01', 'Dimostrazione', 3),
-(3, 3, 2, 2, '2001-06-01', 'Altro', 2);
+(1, 1, 3, 1, '2001-03-01', 'Ricerca e Sviluppo', 6),
+(2, 2, 3, 1, '2001-04-01', 'Management', 4),
+(3, 3, 3, 1, '2001-05-01', 'Dimostrazione', 3),
+(4, 4, 3, 1, '2001-06-01', 'Altro', 2);
+
 
 
 INSERT INTO AttivitaNonProgettuale VALUES
-(0, 0, 'Didattica', 0, '2011-03-15', 6),
-(1, 1, 'Didattica', 0, '2011-05-07', 4),
-(2, 2, 'Didattica', 0, '2012-04-18', 3),
-(3, 3, 'Didattica', 0, '2014-04-01', 4),
-(4, 0, 'Didattica', 0, '2014-04-03', 5);
+(1, 1, 'Didattica', 1, '2011-03-15', 6),
+(2, 2, 'Didattica', 1, '2011-05-07', 4),
+(3, 3, 'Didattica', 1, '2012-04-18', 3),
+(4, 4, 'Didattica', 1, '2014-04-01', 4),
+(5, 1, 'Didattica', 1, '2014-04-03', 5);
+
 
 
 
 INSERT INTO Assenza VALUES
-(0, 0, 'Maternita', '2012-02-01', 4),
-(1, 1, 'Malattia', '2013-03-01', 6),
-(2, 2, 'Chiusura Universitaria', '2014-08-15', 8);
+(1, 1, 'Maternita', '2012-02-01', 4),
+(2, 2, 'Malattia', '2013-03-01', 6),
+(3, 3, 'Chiusura Universitaria', '2014-08-15', 8);
+
+
 
