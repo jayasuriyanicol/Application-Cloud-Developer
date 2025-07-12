1bfe3a873cba160b15262fcf4e1bf60de1c1a9eb
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2025-07-12 17:49:18 +0200

    UPDATE - 12_07_2025 | Adding Persona Function

--- Progettazione 1 - Basi di Dati 1-2/Basi di Dati/Accademia/CreazioneProfili.py
+++ Progettazione 1 - Basi di Dati 1-2/Basi di Dati/Accademia/CreazioneProfili.py
@@ -1,3 +1,43 @@
 '''GENERAZIONE CASUALE DEI PROFILI - SECONDO IL FORMATO SCELTO '''
 
 
+import random
+from random import sample
+from datetime import date, timedelta
+from typing import Any
+
+
+def PersoneRandomiche (N:int) -> set[int]:
+
+    risultato = 'INSERT INTO public.PERSONA(ID, NOME, COGNOME, POSIZIONE, STIPENDIO) VALUES \n'
+      
+    #Attraverso l'utilizzo di SAMPLE limitiam la creazione di duplicati 
+    idPersone = sample(range(10000000), k=N)
+
+    for elemento in range(N):
+        idPersona = idPersone[elemento]
+
+        nome =  str(random.randint(10000,1000000)) 
+        cognome =  str(random.randint(10000,1000000)) 
+
+
+        posizione = random.choice('RIcercatore', 'Professore Associato', 'Professore Ordinario')
+
+
+        match posizione:
+
+            case 'Ricercatore':
+                minimoStipendio, massimoStipendio = 19000, 28000
+
+            case 'Professore Associato' :
+                minimoStipendio, massimoStipendio = 25000, 40000
+
+            case 'Professore Ordinario':
+                minimoStipendio, massimoStipendio = 30000, 100000
+
+        stipendio = random.randint(minimoStipendio,massimoStipendio)
+
+        risultato += f"{idPersona},\'{nome},\'\'{cognome},\'{posizione}\',{stipendio},\n" 
+        print(risultato[:-2]+";" )
+        return risultato
+    
