6c2d3cafebd135c5d03096f6000ad4e3a9c0758e
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2025-07-14 18:32:21 +0200

    UPDATE - 14_07_2025 | Adding Implementation eBuy

--- Progettazione 1 - Basi di Dati 1-2/Progettazione/Progettazione - Esercizi sui Diagrammi/Progetti/eBuy/eBuy/eBuy - Implementazione/eBuy.py
+++ Progettazione 1 - Basi di Dati 1-2/Progettazione/Progettazione - Esercizi sui Diagrammi/Progetti/eBuy/eBuy/eBuy - Implementazione/eBuy.py
@@ -5,11 +5,13 @@
 '''IMPORT per l'implementazione'''
 
 from __future__ import annotations
-import weakref
-from typing import Any, Self
-from TipiDato import *
-from datetime import datetime
 from abc import ABC, abstractmethod
+from typing import *
+from datetime import datetime, date, timedelta
+from TipiDato import *
+import weakref
+
+
 
 
 
@@ -18,16 +20,15 @@ from abc import ABC, abstractmethod
 
 class Utente(ABC):
 
-    _indexUsername: Index[str, Self] = Index("Lista Username") # indice per la lista '_indexusername' 
-    _username: str  # valore <<imm>> {id}
-    _registrazione: datetime  # valore <<imm>>
+    _username : str  #valore <<imm>> {id}
+    _registrazione: datetime.time  #valore <<imm>
 
 
     @abstractmethod
     def __init__(self, username:str, registrazione:datetime.date) -> None:
-
+        
+        #Verifichiamo che venga assegnato un corretto valore per 'username'
         if username:
-            self._indexUsername.add(username, self)
             self._username = username
         else:
             raise AttributeError("ATTENZIONE ! il valore dell'attributo '_username' non può essere nullo (None) o vuoto")
@@ -38,19 +39,11 @@ class Utente(ABC):
 
     def registrazione(self) -> datetime.date:
         return self._registrazione
-
-    @classmethod
-    def all(cls):
-        return cls._indexUsername.all()
-
-    @classmethod
-    def get(cls, username: str):
-        return cls._indexUsername.get(username)
-
+    
+    #Mostriamo i dati dell'utente "Registrato" 
     def __str__(self) -> str:
         return f"L'utente : {self.username()}\nRisulta essere REGISTRATO in data: {self.registrazione()}"
     
-
 
 
 '''Creazione classe PRIVATO (UtentePrivato)'''
@@ -58,24 +51,26 @@ class Utente(ABC):
 
 class Privato(Utente):
 
-    _bidEffettuati: dict[Bid, bid_ut._link] #link associazione con la classe Bid(bid_ut) 
+    _bidEffettuatiUtente: dict[Bid, bid_ut._link] #link associazione con la classe Bid(bid_ut) 
 
     def __init__(self, username:str, registrazione:datetime.date)-> None:
         super().__init__(username, registrazione)
 
-        self._bidEffettuati= dict()
+        self._bidEffettuatiUtente = dict()
 
         '''La classe PRIVATO non presenta attributi specifici'''
 
 
 
     def bid(self) -> frozenset[weakref[bid_ut._link]]:
-        refs = set()
-        for l in self._bidEffettuati.values():
 
-            refs.add(weakref.ref(l))
+        riferimentoBid = set()
 
-        return frozenset(refs)
+        for l in self._bidEffettuatiUtente.values():
+
+            riferimentoBid.add(weakref.ref(l))
+
+        return frozenset(riferimentoBid)
                     
 
 
@@ -83,12 +78,13 @@ class Privato(Utente):
 
         if linkBid.privato() is not self:
 
-            raise ValueError("ATTENZIONE ! Il link non coinvolge me !")
+            raise ValueError("ATTENZIONE ! Il link ERRATTO !")
         
-        if linkBid.bid() in self._bidEffettuati:
+        if linkBid.bid() in self._bidEffettuatiUtente:
 
             raise KeyError(f"ATTENZIONE ! I link duplicati -> ({self}, {linkBid.bid()}) NON sono ammessi !")
-        self._bidEffettuati[linkBid.bid()] = linkBid
+        
+        self._bidEffettuatiUtente[linkBid.bid()] = linkBid
 
 
 
@@ -137,7 +133,7 @@ class Bid:
         
         if linkPrivato.bid() is not self:
 
-            raise ValueError("ATTENZIONE ! Il link non COINVOLGE ME !")
+            raise ValueError("ATTENZIONE ! Il link ERRATO !")
 
         self._privato = linkPrivato.privato()
     
@@ -152,4 +148,257 @@ class Bid:
             raise ValueError("ATTENZIONE ! Il link non COINVOLGE ME !")
 
         self._asta = linkAsta.asta()
+    
+
+    def __hash__(self) -> int:
+        return hash((self._istante, self._privato, self._asta))
+
+    def __eq__(self, other: Any) -> bool:
+        if not isinstance(other, Bid):
+            return False
+        return (self._istante == other._istante and
+                self._privato == other._privato and
+                self._asta == other._asta)
+
+
+
+   
+'''Creazione della classe POST OGGETTO'''
+
+
+class PostOggetto(ABC):
+
+    _descrizione: str
+    _prezzo: RealGEZ
+    _anni_garanzia: IntGEZ
+
+    @abstractmethod
+    def __init__(self, descrizione:str, anni_garanzia:IntGEZ, prezzo:RealGEZ) -> None:
+
+        self.setDescrizione(descrizione)
+        self.setAnniGaranzia (anni_garanzia)
+        self._prezzo = prezzo
+    
+    def descrizione(self) -> str:
+        return self._descrizione
+    
+    def anni_garanzia (self) -> IntGEZ:
+        return self._anni_garanzia
+    
+    def prezzo(self) -> RealGEZ:
+        return self._prezzo
+    
+    def setDescrizione(self, descrizioneEffettiva: str) -> None:
+        self._descrizione = descrizioneEffettiva
+
+    def setAnniGaranzia(self, garanziaEffettiva: IntGEZ) -> None:
+        self._anni_garanzia = garanziaEffettiva
+
+    @abstractmethod
+    def __str__(self) -> str:
+        
+        return f"DETTAGLI OGGETTO\nDESCRIZIONE PRODOTTO: {self.descrizione}\nANNI GARANZIA:{self.anni_garanzia}\nPREZZO PRODOTTO:{self.prezzo}"
+
+
+
+
+'''Creazione della classe ASTA(PostOggetto)'''
+
+
+class Asta(PostOggetto):
+    _prezzo_bid: RealGZ
+    _scadenza: datetime
+    _bidEffetuatiStorico: dict[Bid, asta_bid._link]
+
+    def __init__(self, descrizione: str, anni_garanzia: IntGEZ, prezzo_iniziale: RealGEZ, prezzo_rialzi: RealGZ, scadenza: datetime.time) -> None:
+        
+        self._scadenza = scadenza
+        self._prezzo = prezzo_iniziale
+        self._bidEffetuati = prezzo_rialzi
+        self._bidEffetuatiStorico = dict()
+        
+        super().__init__(descrizione, anni_garanzia, prezzo_iniziale)
+
+    def prezzo_bid(self) -> RealGZ:
+
+        return self._prezzo_bid
+
+    def scadenza(self) -> datetime.time:
+
+        return self._scadenza
+    
+    def bidEffettuatiStorico(self) -> frozenset[weakref[asta_bid._link]]:
+
+        refs = set()
+        for l in self._bidEffettuatiStorico.values():
+
+            refs.add(weakref.ref(l))
+
+        return frozenset(refs)
+        
+    
+    def setPrezzoBid(self, costoEffettivoProdotto: RealGZ) -> None:
+
+        if not self.scaduto():
+
+            self._prezzo_bid = costoEffettivoProdotto
+        
+        else:
+            raise AttributeError("ATTENZIONE ! Non è possibile settare un 'prezzo_bid' ad un'asta che è già terminata !")
+        
+        if self._bidEffetuatiStorico:
+
+            raise AttributeError("ATTENZIONE ! Non è possibile settare un 'prezzo_bid' ad un'asta che ha già uno  o più bid !")
+        else:
+
+            self._prezzo_bid = costoEffettivoProdotto  
+
+
+    def setPrezzo(self, inserimentoPrezzoEffettivo:RealGZ) -> None:
+
+        if not self.scaduto():
+            self._prezzo = inserimentoPrezzoEffettivo
+
+            print("SUCCESSO ! Prezzo inserito correttamente ")
+
+        else:
+            raise AttributeError("ATTENZIONE ! Non è possibile settare un 'prezzo' ad un'asta che è già terminata !")
+        
+        if self._bidEffetuatiStorico:
+
+            raise AttributeError("ATTENZIONE ! Non è possibile settare un 'prezzo' ad un'asta che ha già uno  o più bid !")
+        else:
+
+            self._prezzo_bid = inserimentoPrezzoEffettivo  
+
+    
+    def setScadenza (self, inserimentoScadenzaEffettiva: datetime.time) -> None:
+
+        if self.scaduto():
+
+            raise AttributeError("ATTENZIONE ! Non è possibile settare una 'scadenza' ad un'asta che è già terminata !")
+        
+        elif inserimentoScadenzaEffettiva < datetime.now():
+
+            raise AttributeError("ATTENZIONE ! Non è possibile settare una 'scadenza' ad un'asta che è prima della data attuale !")
+        
+        else:
+            self._scadenza = inserimentoScadenzaEffettiva
+
+        if self._bidEffetuatiStorico:
+
+            raise AttributeError("ATTENZIONE ! Non è possibile settare una 'scadenza' ad un'asta che ha già uno  o più bid !")
+        else:
+
+            self._scadenza = inserimentoScadenzaEffettiva  
+
+
+
+    def _addBid (self, linkBid: asta_bid._link) -> None:
+
+        if linkBid.asta() is not self:
+
+            raise ValueError("ATTENZIONE ! Il link non COINVOLGE ME !")
+        
+        if linkBid.bid() in self._bidEffetuatiStorico:
+
+            raise KeyError(f"ATTENZIONE ! Ci sono due link dupilicati -> ({self}, {linkBid.bid()}) NON sono ammessi !")
+        
+        self._bidEffetuati[linkBid.bid()] = linkBid
+
+
+
+    def scaduto(self) -> bool:
+
+        return self.scadenza() < datetime.now()
+    
+
+    def __str__(self) -> str:
+        return (f"RIEPILOGO ASTA:\nDESCRIZIONE PRODOTTO: {self._descrizione}\nPREZZO ORIGINALE: {self._prezzo:.2f}€\nRIALZO BID MINIMO: {self.prezzo_bid():.2f}€\n"
+        f"SCADENZA ASTA: {self.scadenza()}\n\DURATA GARANZIA PRODOTTO: {self.anni_garanzia()} DI  ANN{'O' if self.anni_garanzia() == 1 else 'I'}\n"
+        f"STATO ASTA: {'ATTIVO' if self.scaduto() else 'SCADUTO'}"
+    )
+
+
+
+'''Creazione del Link Associazione bid_ut (Privato -> Bid | Bid -> Privato)'''
+
+
+class bid_ut:
+
+    class _link: #-> (Privato -> Bid | Bid -> Privato)
+
+        def __init__(self, bid: Bid, privato: Privato) -> None:
+
+            self._bid = bid
+            self._privato = privato
+
+        def bid(self) -> Bid:
+            return self._bid
+
+        def privato(self) -> Privato:
+            return self._privato
+
+
+    @classmethod
+    def _add(cls, bid: Bid, privato: Privato) -> None: 
+
+        link = cls._link(bid, privato) #Link NON rimuovibili 
+        bid._setPrivato(link) #valore <<imm>>  
+        privato._aggiungiBid(link) #valore certamente NON noto alla nascita 
+
+
+    
+    def __hash__ (self) -> str:
+        return hash((self.bid(), self.privato()))
+    
+
+
+    def __eq__(self,other) -> bool | str:
+
+        if not isinstance(self,other) and hash(self) != hash(other):
+            return False
+        return ((self.bid(), self.privato() == (other.bid(), other.privato())))
+
+
+
+
+
+
+'''Creazione Link Associazione asta_bid(BID -> ASTA | ASTA -> BID )'''
+
+class asta_bid:  
+
+    class _link:  #-> (Privato -> Bid | Bid -> Privato)
+        def __init__(self, bid: Bid, asta: Asta) -> None:
+
+            self._bid = bid
+            self._asta = asta
+        
+        def bid(self) -> Bid:
+
+            return self._bid
+        
+        def asta(self) -> Asta:
+
+            return self._asta
+        
+
+    
+    @classmethod
+    def _add(cls, bid: Bid, asta: Asta) -> None:
+
+        link = cls._link(bid, asta) #Link NON rimuovibili 
+        bid._setAsta(link) #valore <<imm>> {id}
+        asta._addBid(link)  #valore certamente NON noto alla nascita
+
+    def __hash__(self) -> int:
+            return hash((self.bid(), self.asta()))
+
+    def __eq__(self, other: Any) -> bool:
+            if not isinstance(other, asta_bid._link):
+                return False
+            return (self.bid() == other.bid() and self.asta() == other.asta())
+
+
 
