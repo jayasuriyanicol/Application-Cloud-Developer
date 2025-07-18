faf17ce284ab9abcfc51dcd4a4ca70da0f1045b6
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2025-07-18 19:01:53 +0200

    UPDATE - 18_07_2025 | Adding Creation Third Query of DB CIelo

--- Progettazione 1 - Basi di Dati 1-2/Basi di Dati/Cielo/QueryCielo.sql
+++ Progettazione 1 - Basi di Dati 1-2/Basi di Dati/Cielo/QueryCielo.sql
@@ -4,7 +4,7 @@
 
 -- 1 | Quali sono i voli (codice e nome delle compagnia) la cui durata supera le 3 ore?
 
-SELECT v.codice, v.comp
+SELECT v.codice codice_volo, v.comp compagnia
 
 FROM Compagnia AS com, Volo AS v
 
@@ -14,7 +14,7 @@ WHERE com.nome = v.comp AND v.durataMinuti > 180;
 -- 2 | Quali sono le compagnie che hanno voli che superano le 3 ore?
 
 
-SELECT DISTINCT v.comp
+SELECT DISTINCT v.comp compoagnia
 
 FROM Compagnia com, Volo v
 
@@ -25,7 +25,11 @@ ORDER BY v.comp DESC; -- Nel caso vogliamo l'output come da "Risultati Attesi" s
 
 
 
--- 3 | Quali sono i voli(codice e nome della compagnia) che partono dall'aeroporto con codice 'CIA'?
+-- 3 | Quali sono i voli (codice e nome della compagnia) che partono dall'aeroporto con codice 'CIA'?
 
 
+SELECT v.codice codice_volo, v.comp compagnia
 
+FROM Volo v, ArrPart arp 
+
+WHERE v.codice = arp.codice AND v.comp = arp.comp AND partenza = 'CIA';
