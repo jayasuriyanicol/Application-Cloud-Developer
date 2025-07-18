4ce03b7d59a9db250da2bdd933cf9bf26d6fef83
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2025-07-18 18:30:09 +0200

    UPDATE - 18_07_2025 | Updating Cielo DB Order Creation DB

--- Progettazione 1 - Basi di Dati 1-2/Basi di Dati/Cielo/Cielo.sql
+++ Progettazione 1 - Basi di Dati 1-2/Basi di Dati/Cielo/Cielo.sql
@@ -20,6 +20,42 @@ CREATE DOMAIN CodIATA CHAR(3);
 -- CREAZIONE DELLE TABELLE - PRE MANIPOLAZIONE
 
 
+CREATE TABLE Compagnia(
+
+    nome StringaM NOT NULL,
+    annoFondaz PosInteger NULL,
+
+    PRIMARY KEY(nome)
+);
+
+
+
+CREATE TABLE Aeroporto(
+
+    codice CodIATA NOT NULL,
+    nome StringaM NOT NULL,
+
+    PRIMARY KEY (codice)
+);
+
+
+
+CREATE TABLE LuogoAeroporto(
+
+    aeroporto CodIATA NOT NULL,
+    citta StringaM NOT NULL,
+    nazione StringaM NOT NULL,
+
+    PRIMARY KEY(aeroporto),
+    FOREIGN KEY (aeroporto) REFERENCES Aeroporto(codice) deferrable
+);
+
+
+
+ALTER TABLE Aeroporto ADD FOREIGN KEY(codice) REFERENCES LuogoAeroporto(aeroporto) deferrable;
+
+
+
 CREATE TABLE Volo(
 
     codice PosInteger NOT NULL,
@@ -49,32 +85,6 @@ CREATE TABLE ArrPart(
 ALTER TABLE ArrPart ADD FOREIGN KEY (codice,comp) REFERENCES Volo(codice,comp) deferrable;
 
 
-CREATE TABLE Aeroporto(
-
-    codice CodIATA NOT NULL,
-    nome StringaM NOT NULL,
-
-    PRIMARY KEY (codice)
-);
 
 
-ALTER TABLE Aeroporto ADD FOREIGN KEY(codice) REFERENCES LuogoAeroporto(aeroporto) deferrable;
 
-CREATE TABLE LuogoAeroporto(
-
-    aeroporto CodIATA NOT NULL,
-    citta StringaM NOT NULL,
-    nazione StringaM NOT NULL,
-
-    PRIMARY KEY(aeroporto),
-    FOREIGN KEY (aeroporto) REFERENCES Aeroporto(codice) deferrable
-);
-
-
-CREATE TABLE Compagnia(
-
-    nome StringaM NOT NULL,
-    annoFondaz PosInteger NULL,
-
-    PRIMARY KEY(nome)
-);
