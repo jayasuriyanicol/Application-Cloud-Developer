db97b801e2ca9f84b6c2d8a75978777ab19370e4
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2025-07-22 17:25:05 +0200

    UPDATE - 22_07_2025 | Adding Implementation of  Operations

---
+++ Progettazione 1 - Basi di Dati 1-2/Progettazione/Progettazione - Esercizi sui Diagrammi/Progetti/eBuy/eBuy/eBuy - Implementazione/Implementazione Operazioni/Progetto eBuy - Implementazione Operazioni.py
@@ -0,0 +1,56 @@
+'''IMPLEMENTAZIONI DELLE CLASSI in Python'''
+
+
+
+def ultimo_bid(self) -> Bid | None:
+
+    if not self._bid: 
+        return None
+    max_b = None
+
+    for l in self._bids.values():
+        if l.bid.istante <= datetime.now():
+            if  max_b == None:
+                max_b = l.bid
+            else:
+                if l.bid().istante() > max_b.istante():
+
+                    max_b = l.bid()
+    return max_b
+
+
+def scaduta(self,i:datetime) -> bool:
+
+    if self.scadenza() < i:
+       return True
+    return False
+
+
+def vincitore(self) -> UtentePrivato | None:
+
+    if not self.scaduta(datetime.now()):
+        return None
+    
+    b = self.ultimo_bid(datetime.now())
+
+    if b == None:
+
+        return None
+    return b.bid_ut().utente_privato()
+
+
+def prezzo(self, i:datetime) -> FloatGEZ:
+
+     B = [] 
+
+     for l in self.asta_bid:
+
+        if l.bid().istante() <= i:
+
+             B.append(l.bid())
+    
+     N= len(B)
+
+     result:FloatGEZ = self.prezzo  + N * self.prezzo_bid
+
+     return result   
