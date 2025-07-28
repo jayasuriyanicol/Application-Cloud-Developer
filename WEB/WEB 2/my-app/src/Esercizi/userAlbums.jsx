8c2f4703d9317d909dc74afc60d45333e1b9cdbe
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2025-07-28 12:16:14 +0200

    UPDATE - 28_07_2025 | Adding Creation File "AppVite"

---
+++ WEB/WEB 2/my-app/src/Esercizi/userAlbums.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+
+const userAlbums = () =>{
+
+    //fetch da https://jsonplaceholder.typicode.com/users
+    //useState users
+    //useState userSelected 
+
+    return (
+        <div className='container'>
+            <div className="row">
+                <div className='col-6'>
+                    <select>
+                        <option value="">Seleziona l'utente</option>
+
+                    </select>
+                    qui stampiamo il dato 
+
+                </div>
+            </div>
+        </div>
+    )
