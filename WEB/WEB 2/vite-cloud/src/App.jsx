6dfd92937cfddc9024525fa105e827073af2f895
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2025-07-31 19:43:39 +0200

    UPDATE - 31 - 07 - 2025 | Adding Vite Application Stuff

---
+++ WEB/WEB 2/vite-cloud/src/App.jsx
@@ -0,0 +1,14 @@
+import UserCrud from './Esercizi Vite/UserCrud'
+import UserAlbum from './Esercizi Vite/UserAlbums'
+// oppure
+// import UserAlbums from './UserAlbums'
+
+function App() {
+  return (
+    <div className="container mt-5">
+      <UserCrud />
+    </div>
+  )
+}
+
+export default App;
