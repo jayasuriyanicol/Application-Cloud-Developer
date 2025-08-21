f667b04b07afb0fd9c9f20b0bfad097f3da5f031
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2025-08-21 11:32:42 +0200

    UPDATE - 21_08_2025 | Adding Types for OrdiniFatture1

---
+++ Progettazione 1 - Basi di Dati 1-2/Basi di Dati/Ordini e Fatture /Ordini e Fatture 1/Tipi OrdinieFatture.sql
@@ -0,0 +1,62 @@
+-- CREAZIONE DEL DB ORIDNI E FATTURE 1
+
+
+CREATE DATABASE OrdiniFatture1;
+
+\c OrdiniFaitture1
+
+BEGIN TRANSACTION;
+
+SET CONSTRAINTS ALL DEFERRED;
+
+
+-- Creazione dei DOMINI NUMERICI
+
+CREATE DOMAIN RealGEZ as REAL
+CHECK(value >= 0);
+
+CREATE DOMAIN IntGEZ as INTEGER
+CHECK( value >= 0);
+
+CREATE DOMAIN Real0_1 as REAL
+CHECK( value >= 0 and value <= 1);
+
+
+-- Creazione dei DOMINI 
+
+CREATE DOMAIN Stringa as VARCHAR;
+
+CREATE DOMAIN CodiceFiscale as Stringa
+CHECK (value ~ '[A-Z]{6}[0-9]{2}[A-Z][0-9]{2}[A-Z][0-9]{3}[A-Z]$');
+
+CREATE DOMAIN PartitaIva as Stringa
+CHECK (value ~ '^[0-9]{11}$');
+
+CREATE DOMAIN Telefono as Stringa
+CHECK (value ~ '^\+[1-9][0-9]{0,2} [0-9]{6,13}$');
+
+create domain Email as Stringa
+CHECK (value ~ '^[\w\.-]+@[\w\.-]+\.\w{2,}$');
+
+
+-- Creazione dei TIPI 
+
+CREATE DOMAIN Indirizzo AS(
+ 
+ via Stringa,
+ civico Stringa,
+ CAP  CHAR(5)
+    
+);
+
+
+CREATE TYPE Stato as ENUM (
+
+    'in preparazione ',
+    'inviato',
+    'da saldare',
+    'saldato'
+);
+
+
+COMMIT;
