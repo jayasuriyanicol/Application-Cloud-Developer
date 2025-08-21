4d08b68dcc6f6dfc54967f05ff0be205b6664d97
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2025-08-21 14:37:55 +0200

    UPDATE - 21_08_2025 | Adding Third Exercise of August React Exercises

---
+++ WEB/WEB 2/vite-cloud/src/Esercizi Vite/Esercizi React Agosto /Esercizio 3/piatti.js
@@ -0,0 +1,8 @@
+const piatti =[ 
+
+   {id : 1, nome: "Risotto alla Pescatora", prezzo: 6.50 },
+   {id : 2, nome: "Pizza Patate e Salsiccie", prezzo: 5.50},
+   {id : 3, nome: "Frittura di Pesce", prezzo: 9.50},
+   {id : 4, nome: "Torta al Limone", prezzo: 6.45 }
+
+];
