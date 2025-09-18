e5c5539c0ce21beb52a7d2dffef5ef10f8d2ae1a
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2025-09-18 11:01:45 +0200

    UPDATE - 18_09_2025 | Ultimating the DRIVER PROGRAMM code with comments

--- Python 1_4/Lezione_17/Esercitazione Classi e Ereditarietà/codiceDriver.py
+++ Python 1_4/Lezione_17/Esercitazione Classi e Ereditarietà/codiceDriver.py
@@ -20,12 +20,60 @@ Scrivere, infine, il codice driver che gestisca due dottori e due liste di pazie
 
 from paziente import Paziente
 from dottore import Dottore
-from persona import Persona
 from fatture import Fattura
 
-
-
+#Procediamo alla creazione dei due medici per l'ospedale
 primoMedico = Dottore("Mario", "Rossi", "Chirurgo", "CA345")
-Dottore.setAge(30)
+Dottore.setAge(48)
 
-secondoMedico = Dottore("Francesco", "Bianchi", "Medico")
+secondoMedico = Dottore("Francesco", "Bianchi", "Medico","FG543")
+Dottore.setAge(31)
+
+#Successivamente andiamo alla creazione dei primi 3 pazienti che saranno utenti del PRIMO MEDICO, aggiungendoli nella lista del PRIMO MEDICO
+primoPaziente = Paziente("Luca", "Verdi", "ID001")
+primoPaziente.setAge(35)
+
+secondoPaziente = Paziente("Anna", "Neri", "ID002")
+secondoPaziente.setAge(28)
+
+terzoPaziente = Paziente("Marco", "Gialli", "ID003")
+terzoPaziente.setAge(50)
+
+listaPazientiPrimoMedico = [primoPaziente,secondoPaziente,terzoPaziente]
+
+
+#Infine, andiamo alla creazione del ultimno paziente che sarà utente del SECONDO MEDICO, aggiungendolo nella lista del SECONDO MEDICO
+quartoPaziente = Paziente("Claudia", "Bianchi", "ID004")
+quartoPaziente.setAge(22)
+
+listaPazientiSecondoMedico = [quartoPaziente]
+
+#Presentiamo i medici con il metodo greet, come richiesto dalla consegna
+print("\n   PRESENTAZIONE MEDICI  ")
+primoMedico.doctorGreet()
+secondoMedico.doctorGreet()
+
+#Andiamo con la FATTURIZZAZIONE dei due MEDICI
+primaFattura = Fattura(listaPazientiPrimoMedico, primoMedico)
+secondaFattura = Fattura(listaPazientiSecondoMedico, secondoMedico)
+
+#Stampiamo i salari per i due MEDICI
+print("\n  SALARI INIZIALI  ")
+print(f"Salario del PRIMO MEDICO: {primaFattura.getSalary()} euro!")
+print(f"Salario del SECONDO MEDICO: {secondaFattura.getSalary()} euro!")
+
+
+#Andiamo con la rimozione del SECONDO PAZEINTE del PRIMO MEDICO procedendo all'inserimento della lista del SECONDO MEDICO
+primaFattura.removePatient("ID002")
+secondaFattura.addPatient(secondoPaziente)
+
+
+#Infine, ristampiamo i salari AGGIORNATI dopo la "sostituzione" del PAZIENTE
+print("\n  SALARI DOPO SPOSTAMENTO ")
+print(f"Salario del PRIMO MEDICO: {primaFattura.getSalary()} euro!")
+print(f"Salario del SECONDO MEDICO: {secondaFattura.getSalary()} euro!")
+
+
+#In conclusione, andiamo a calcolare il GUADAGNO OSPEDALIERO con le due FATTURE dei relativi medici
+guadagnoComplessivoOspedaliero = primaFattura.getSalary() + secondaFattura.getSalary()
+print(f"\nIn TOTALE, l'ospedale ha incassato {guadagnoComplessivoOspedaliero} EURO !")
