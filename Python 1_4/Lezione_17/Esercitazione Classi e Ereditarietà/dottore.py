2bfdc2e0ec505212072e61e9e51ac9d8c6b61787
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2025-09-18 10:33:36 +0200

    UPDATE - 18_09_2025 | Modyfing to a smooth Output for the class Dottore

--- Python 1_4/Lezione_17/Esercitazione Classi e Ereditarietà/dottore.py
+++ Python 1_4/Lezione_17/Esercitazione Classi e Ereditarietà/dottore.py
@@ -15,81 +15,86 @@ Un dottore ha un nome, un cognome, un età, definiti dalla classe Persona, una s
     doctorGreet():tale metodo richiama la funzione greet() della classe Persona. Poi, stampa il seguente saluto "Sono un medico {specializzazione}"
 '''
 
+
 from persona import Persona
 
 class Dottore(Persona):
 
-    def __init__(self, first_name:str, last_name:str,specialization:str, parcel:str):
+    def __init__(self, first_name: str, last_name: str, specialization: str, parcel: float):
+
         super().__init__(first_name, last_name)
 
-        self.specialization = specialization
-        self.parcel = parcel
+        try:
+            self.__specialization = str(specialization)
 
-    try:
+        except ValueError:
 
-        specialization = str(specialization)
-    
-    except ValueError:
-        print("ATTENZIONE ! il valore inserito per la SPECIALIZZAZIONE non è una STRINGA !")
-
-        specialization = None
-    
-    try:
-
-        parcel = int(parcel)
-    
-    except ValueError:
-        print("ATTENZIONE ! il valore inserito per la PARCELLA non è valido solo FLOAT !")
-
-        parcel = None
-    
-
-    def setSpecialization(self,specialization:str) -> None:
+            print("ATTENZIONE ! la SPECIALIZZAZIONE non è una STRINGA!")
+            self.__specialization = None
 
         try:
+            self.__parcel = float(parcel)
 
-            specialization = str(specialization)
-    
         except ValueError:
-         print("ATTENZIONE ! il valore inserito per la SPECIALIZZAZIONE non è una STRINGA !")
 
-    
-    def setParcel(self,parcel:float) -> None:
+            print("ATTENZIONE ! la PARCELLA non è un FLOAT!")
+            self.__parcel = None
+
+
+    def setSpecialization(self, specialization: str) -> None:
 
         try:
+            self.__specialization = str(specialization)
 
-            parcel = float(parcel)
-    
         except ValueError:
-         
-         print("ATTENZIONE ! il valore inserito per la PARCELLA non è valido solo FLOAT !")
-    
+
+            print("ATTENZIONE ! il valore inserito per la SPECIALIZZAZIONE non è una STRINGA!")
+
+
+
+    def setParcel(self, parcel: float) -> None:
+
+        try:
+            self.__parcel = float(parcel)
+        except ValueError:
+
+            print("ATTENZIONE ! la PARCELLA non è un FLOAT!")
+
+
     def getSpecialization(self) -> str:
 
-        return self.specialization
+        return self.__specialization
     
-    def getParcel(self) -> str:
 
-        return self.parcel
+    def getParcel(self) -> float:
+
+        return self.__parcel
     
-    def isAValidDoctor(self) -> str:
 
-        if self.age >= 30:
+    def isAValidDoctor(self) -> None:
 
-            print(f"Doctor {self.first_name} {self.last_name} is valid !")
+        if self.getAge() is not None and self.getAge() >= 30:
+
+            print(f"Doctor {self.getName()} {self.getLastName()} is VALID!")
+
         else:
+            print(f"Doctor {self.getName()} {self.getLastName()} is NOT VALID!")
 
-            print(f"Doctor {self.first_name} {self.last_name} is not valid !")
-    
-    def doctorGreet(self) -> str:
 
-        print (f"Sono un medico {self.specialization}")
-        return self.greet() 
+    def doctorGreet(self) -> None:
+
+        self.greet() 
         
-    
+        print(f"Sono un medico {self.__specialization}")
 
 
-'''DRIVER PROGRAMM'''
 
+
+'''DRIVER PROGRAMM, per testare le funzionalità della classe Dottore'''
 
 dottore = Dottore("Nicol", "Jayasuriya", "Chirurgo", 2050.00)
+
+dottore.isAValidDoctor()
+dottore.doctorGreet()
+print(f"Specializzazione: {dottore.getSpecialization()}")
+print(f"Parcella: {dottore.getParcel():.2f} €")
