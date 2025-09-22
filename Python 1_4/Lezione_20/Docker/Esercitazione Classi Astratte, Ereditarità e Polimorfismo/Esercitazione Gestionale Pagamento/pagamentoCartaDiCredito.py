dfaee61410f1133c3488e8b04be228b847db78f2
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2025-09-22 11:50:00 +0200

    UPDATE - 22_09_2025 | Adding New Class PagamentoCartaDiCredito on Gestionale Pagamento

---
+++ Python 1_4/Lezione_20/Docker/Esercitazione Classi Astratte, Ereditarità e Polimorfismo/Esercitazione Gestionale Pagamento/pagamentoCartaDiCredito.py
@@ -0,0 +1,51 @@
+'''
+Si definisca una classe PagamentoCartaDiCredito derivata anch'essa da Pagamento 
+e che definisce l'importo. 
+Questa classe deve contenere gli attributi per il nome del titolare della carta, la data di scadenza, e il numero della carta di credito. Infine, si ridefinisca il metodo dettagliPagamento() per includere tutte le informazioni della carta di credito oltre 
+all'importo del pagamento.
+'''
+
+from pagamento import Pagamento
+
+class PagamentoCartaDiCredito(Pagamento):
+
+
+    def __init__(self,nomeTitolare:str,dataScadenza:str, numeroCartaCredito:str) -> None:
+        super().__init__()
+
+        self.__nomeTitolare = nomeTitolare
+        self.__dataScadenza = dataScadenza
+        self.__numeroCartaCredito = numeroCartaCredito
+    
+
+    def setNomeTitolare(self, nomeTitolare:str) -> None:
+
+        self.__nomeTitolare = nomeTitolare
+    
+    def setDataScadenza(self, dataScadenza:str) -> None:
+
+        self.__dataScadenza = dataScadenza
+    
+    def setNumeroCartaCredito(self, numeroCarta:str) -> None:
+
+        self.__numeroCartaCredito = numeroCarta
+
+    
+    def getNomeTitolare(self) -> str:
+
+        return self.__nomeTitolare
+    
+    def getDataScadenza (self) ->str:
+        return self.__dataScadenza
+    
+    def getNumeroCartaCredito (self) ->str:
+        return self.__numeroCartaCredito
+    
+   
+    def dettagliPagamento(self):
+        
+        print("\n--- Pagamento con Carta di Credito ---")
+        print(f"\nNome sulla carta: {self.__nomeTitolare}")
+        print(f"Data di scadenza: {self.__dataScadenza}")
+        print(f"Numero della carta: {self.__numeroCartaCredito}")
+        super().dettagliPagamento()
