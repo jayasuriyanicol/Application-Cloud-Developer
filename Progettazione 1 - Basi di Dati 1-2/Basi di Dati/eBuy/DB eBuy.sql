fac567d3a6456eaf43bbfcf04befe7338118b90a
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2025-09-22 17:59:59 +0200

    UPDATE - 22_09_2025 | Pre-Ultimation of DB eBuy with Foreign Keys and other stuff

--- Progettazione 1 - Basi di Dati 1-2/Basi di Dati/eBuy/DB eBuy.sql
+++ Progettazione 1 - Basi di Dati 1-2/Basi di Dati/eBuy/DB eBuy.sql
@@ -7,14 +7,15 @@ SET CONSTRAINTS ALL DEFERRED;
 CREATE TABLE Categoria (
 
     nome Stringa PRIMARY KEY,
-    super Stringa
+    super Stringa,
 
     CHECK (nome <> super )
 
 );
 
+
 ALTER TABLE Categoria 
-ADD CONSTRAINTS fk_categoria_super
+ADD CONSTRAINT fk_categoria_super
 FOREIGN KEY (super) REFERENCES categoria(nome);
 
 
@@ -24,59 +25,24 @@ CREATE TABLE Utente(
     username Stringa PRIMARY KEY,
     registrazione TIMESTAMP NOT NULL
 
-);
-
-
-CREATE TABLE PostOggetto(
-
-    descrizione Stringa NOT NULL,
-    pubblicazione TIMESTAMP NOT NULL,
-    ha_feedback BOOLEAN,
-    voto Voto,
-    commento Stringa,
-    istante_feedback TIMESTAMP NOT NULL,
-    id PRIMARY KEY
-
-    FOREIGN KEY Utente REFERENCES utente(username)
-);
-
-
-CREATE TABLE MetodoPagamento(
-
-    nome Stringa PRIMARY KEY NOT NULL
-
-    FOREIGN KEY (met_post) REFERENCES PostOggetto(id)
-);
-
-
-
-CREATE TABLE PostOggettoUsato(
-
-    condizione Condizione NOT NULL,
-    anni_garanzia IntGEZ NOT NULL
-
-
-    FOREIGN KEY (poi_isa_po) REFERENCES PostOggetto(id)
-
+    --Per utilizzare la funzione 'Disjoint & Complete' possiamo utilizzare questo CHECK
+    --dato che in PostgreSQL non vi è un'opzione specifica ma ELEMENTARMENTE possiamo utilizzare
+    --solo dei CHECK per ovviare a questa situazione.
+    tipologiaUtente VARCHAR NOT NULL,
+   
+    CHECK(tipologiaUtente IN ('Privato', 'Venditore'))
 
 );
+
 
 
 CREATE TABLE VenditoreProfessionale(
 
-    vetrina Url PRIMARY KEY
+    vetrina Url UNIQUE NOT NULL,
 
-    
+    vp_isa_ut Stringa PRIMARY KEY,
 
-
-);
-
-
-CREATE TABLE PostOggettoNuovo(
-
-    anni_garanzia IntG1 NOT NULL
-
-    FOREIGN KEY (pon_isa_po) REFERENCES PostOggetto(id)
+    FOREIGN KEY (vp_isa_ut) REFERENCES Utente(username)
 
 
 );
@@ -84,34 +50,133 @@ CREATE TABLE PostOggettoNuovo(
 
 CREATE TABLE Privato(
 
+    pr_isa_ut Stringa PRIMARY KEY,
+
+    FOREIGN KEY (pr_isa_ut) REFERENCES Utente(username)
 
 );
+
+
+
+CREATE TABLE MetodoPagamento(
+
+    nome Stringa PRIMARY KEY NOT NULL
+
+);
+
+
+
+
+CREATE TABLE PostOggetto(
+    
+    id SERIAL PRIMARY KEY,
+    descrizione Stringa NOT NULL,
+    pubblicazione TIMESTAMP NOT NULL,
+    ha_feedback BOOLEAN,
+    voto Voto,
+    commento Stringa,
+    istante_feedback TIMESTAMP NOT NULL,
+
+    Utente Stringa NOT NULL,
+    Categoria Stringa NOT NULL,
+
+    FOREIGN KEY (Utente) REFERENCES utente(username),
+    FOREIGN KEY (Categoria) REFERENCES categoria(nome),
+
+    CHECK((ha_feedback = True AND voto IS NOT NULL)
+      OR  (ha_feedback = False AND voto IS NULL AND commento IS NULL) )
+);
+
+
+CREATE TABLE met_post (
+
+    metodo Stringa NOT NULL,
+    post   INTEGER NOT NULL
+
+    PRIMARY KEY (metodo,post),
+    FOREIGN KEY (metodo) REFERENCES MetodoPagamento(nome),
+    FOREIGN KEY (post) REFERENCES PostOggetto(id)
+
+);
+
+
+
+CREATE TABLE PostOggettoNuovo(
+
+    anni_garanzia IntG1 NOT NULL,
+
+    pon_isa_po INTEGER PRIMARY KEY,
+    pubblica_nuovo Stringa NOT NULL,
+
+    FOREIGN KEY (pon_isa_po) REFERENCES PostOggetto(id),
+    FOREIGN KEY(pubblica_nuovo) REFERENCES VenditoreProfessionale(vp_isa_ut)
+);
+
+
+CREATE TABLE PostOggettoUsato(
+
+    condizione Condizione NOT NULL,
+    anni_garanzia IntGEZ NOT NULL
+
+    pou_isa_po INTEGER PRIMARY KEY,
+    FOREIGN KEY (pou_isa_po) REFERENCES PostOggetto(id)
+
+);
+
+
+
 
 
 CREATE TABLE Asta(
 
     prezzo_base RealGEZ NOT NULL,
     prezzo_bid RealGZ NOT NULL,
-    scadenza TIMESTAMP NOT NULL
+    scadenza TIMESTAMP NOT NULL,
+
+    asta_isa_po INTEGER PRIMARY KEY,
+    FOREIGN KEY (asta_isa_po) REFERENCES PostOggetto(id)
 
 
 );
 
 CREATE TABLE Bid(
 
-    codice SERIAL PRIMARY KEY NOT NULL,
-    istante TIMESTAMP NOT NULL
+    codice SERIAL PRIMARY KEY,
+    istante TIMESTAMP NOT NULL,
+
+
+    
+    asta_bid INTEGER NOT NULL,
+
+    UNIQUE(istante,asta_bid),
+
+    bid_ut Stringa NOT NULL,
+
+    FOREIGN KEY(asta_bid) REFERENCES Asta(asta_isa_po),
+    FOREIGN KEY(bid_ut) REFERENCES Privato(pr_isa_ut)
 
 );
+
+
+
+CREATE TABLE CompraloSubito(
+
+    prezzo RealGZ NOT NULL,
+
+    cs_isa_po INTEGER PRIMARY KEY,
+
+    FOREIGN KEY(cs_isa_po) REFERENCES PostOggetto(id)
+);
+
 
 
 CREATE TABLE Acquirente(
 
-    istante TIMESTAMP NOT NULL
+    acqu_isa_ut Stringa PRIMARY KEY,
+    istante TIMESTAMP NOT NULL,
 
+    FOREIGN KEY(acqu_isa_ut) REFERENCES Utente(username)
 );
 
 
-
-
-
+COMMIT;
