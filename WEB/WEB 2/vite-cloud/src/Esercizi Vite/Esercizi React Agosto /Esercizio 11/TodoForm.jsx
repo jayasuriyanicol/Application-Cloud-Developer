9323b9fd6bfc335c1b64053f45ee6a72afd7859a
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2025-09-24 18:44:19 +0200

    UPDATE - 24_09_2025 | Updating ToDoForm of Exercise number 11 of Esericzi React Agosto

--- WEB/WEB 2/vite-cloud/src/Esercizi Vite/Esercizi React Agosto /Esercizio 11/TodoForm.jsx
+++ WEB/WEB 2/vite-cloud/src/Esercizi Vite/Esercizi React Agosto /Esercizio 11/TodoForm.jsx
@@ -1,25 +1,8 @@
-import { useState } from "react";
+import React from 'react'
 
-const TodoForm = ({ handleAddTask }) => {
-  const [text, setText] = useState("");
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    if (!text.trim()) return;
-    handleAddTask(text);
-    setText(""); // reset input
-  }
-
+const TodoForm = () => {
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        placeholder="Aggiungi un nuovo task..."
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-      />
-      <button type="submit">Aggiungi</button>
-    </form>
+    <div>TodoForm</div>
   );
 };
 
