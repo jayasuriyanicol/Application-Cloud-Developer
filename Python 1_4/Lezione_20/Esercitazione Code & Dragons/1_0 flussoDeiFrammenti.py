74b6113d5d241bfda721e8d8fd2ed893845d4dda
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2025-09-26 21:11:31 +0200

    ADDING - 26_09_2025 | Adding New FiolderCode & Dragons

---
+++ Python 1_4/Lezione_20/Esercitazione Code & Dragons/1_0 flussoDeiFrammenti.py
@@ -0,0 +1,39 @@
+'''
+CAPITOLO 1 - Esercizio numero 1
+
+Flusso dei Frammenti
+
+
+Nel Laboratorio Reale i reagenti sono dispersi. Misura la quantità totale con `sum_list(nums)`, che somma gli interi in `nums`; 
+se non c'è nulla, restituisci `0`. Mantieni la firma e lascia che gli esperimenti (i test) riescano.
+
+'''
+
+
+def sum_list(nums:list[int]) -> int:
+
+    sommaNumeri = 0
+
+    if len(nums) == 0:
+
+        return 0
+
+    else:
+
+        for numeroPresente in nums:
+
+            sommaNumeri += numeroPresente
+        return sommaNumeri
+
+'''Nel caso volessimo utilizzare una formula più ridotta possiamo adottare:'''
+
+def sum_list(nums:list[int]) -> int:
+
+    return sum(nums) if nums else 0
+
+
+
+print(sum_list([1,2,3,4]))
+
+
+
