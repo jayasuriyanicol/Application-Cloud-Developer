6219d6359d7ea8950478701ecd8092516ec2ad89
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2025-10-09 18:51:36 +0200

    UPDATE - 09_10_2025 | Updating  adding sample function and comments

--- Python 1_4/Utility /decoratorCronometro.py
+++ Python 1_4/Utility /decoratorCronometro.py
@@ -1,5 +1,16 @@
 #Example of using a Decorator to take the time/check the time of a function
 
+#Sample usage function of a timer 
+def timer(func):
+
+    def wrapper():
+        import time
+        start: float = time.time()
+        func()
+        print(time.time() - start)
+    return wrapper
+
+
 #Creation of a function Cronometro with usage of *args
 from typing import Callable, Any
 
@@ -13,6 +24,9 @@ def cronometro(function:Callable) -> Callable:
                 print(f"Tempo strascorso: {time() -start}")
         return wrapper
 
+
+
+#Cronometro as a decorator, used to calcolate a 'large' function of a cicle for to in order to test it
 @cronometro
 def testCronometro(num:int) -> None:
         
