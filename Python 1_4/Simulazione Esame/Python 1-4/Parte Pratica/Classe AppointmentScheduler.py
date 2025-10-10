9ed1d4a04e6120a3bc5e8bbc2188b47ef84f2f25
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2025-10-10 18:38:28 +0200

    UPDATE - 10_10_2025 | Updating Classe AppointmentScheduler with the correct solution and a Driver Programm

--- Python 1_4/Simulazione Esame/Python 1-4/Parte Pratica/Classe AppointmentScheduler.py
+++ Python 1_4/Simulazione Esame/Python 1-4/Parte Pratica/Classe AppointmentScheduler.py
@@ -42,3 +42,105 @@ Restituisce il dict dell'appuntamento o "Errore: appuntamento non trovato."
 '''
 
 
+class AppointmentScheduler:
+
+
+    def __init__(self)-> None:
+
+        self.appointments:dict[str,dict] = {} 
+
+        '''
+        Esempio di DICT:
+
+        {'A01': {'data': '05 Giugno 2023', 'programmato' : 'false' }
+
+        Esempio di dizionario, formato da una chiave str e da un dizionario annidato al suo interno con chiavi 'data' e 'programmato'
+        
+        '''  
+
+
+    def schedule_appointment(self,app_id:str,data:str) -> dict | str:
+
+        if app_id in self.appointments:
+
+            return "Errore: appuntamento esiste già"
+        else:
+
+            self.appointments[app_id] = {"data": data, "programmato" : True}
+        return {app_id: self.appointments[app_id]} 
+    
+
+
+    def reschedule_appointment(self,app_id:str,nuova_data:str) -> dict | str:
+
+        if app_id not in self.appointments:
+
+            return "Errore: appuntamento non trovato"
+        else:
+         self.appointments[app_id]["data"] =  nuova_data 
+
+        return { app_id: self.appointments[app_id]} 
+    
+
+    
+    def cancel_appointment(self,app_id:str) -> dict | str:
+
+        if app_id not in self.appointments:
+            return "Errore: appuntamento non trovato"
+        else:
+            self.appointments[app_id]["programmato"] = False
+
+        return {app_id : self.appointments[app_id]} 
+    
+   
+
+    def remove_appointment(self, app_id: str) -> dict | str:
+
+        if app_id not in self.appointments:
+            return "Errore: appuntamento non trovato."
+        else:
+          appuntamentoRimosso = {app_id: self.appointments.pop(app_id)}
+
+        return appuntamentoRimosso
+
+    
+
+    def list_appointments(self)-> list[str]:
+
+        listaId:list[str] = []
+
+        for elem in self.appointments.keys():
+
+            listaId.append(elem)  
+        return listaId
+    
+
+    
+    
+    def get_appointment(self,app_id:str) -> dict | str:
+
+        if app_id in self.appointments:
+
+                return {app_id: self.appointments[app_id]} 
+            
+        else: 
+                return "Errore: appuntamento non trovato"
+    
+
+
+'''DRIVER PROGRAMM'''
+
+sched = AppointmentScheduler()
+
+print(sched.schedule_appointment("A01", "10 Ottobre 2025"))
+print(sched.schedule_appointment("A02", "12 Ottobre 2025"))
+
+print(sched.reschedule_appointment("A02", "11 Ottobre 2025"))
+
+print(sched.cancel_appointment("A01"))
+
+print(sched.get_appointment("A01"))
+
+print(sched.remove_appointment("A01"))
+
+print(sched.list_appointments())
