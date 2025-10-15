931392db33e46d3fc1d9475ade81bae9aa7d457e
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2025-10-15 18:13:32 +0200

    ADD - 15_101_2025 | Adding NascondiMostra UseState basic exercises with new folder

---
+++ WEB/WEB 2/vite-cloud/src/Esercizi Vite/Esercizi React Ottobre/Esercizi UseState/NascondiMostra.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { useState } from "react";
+
+
+const NascondiMostra = () =>{
+
+const [mostraTesto, setMostraTesto] =  useState (false)
+
+
+function mostraContenuto  (){
+
+    setMostraTesto(!mostraTesto)
+
+
+}
+
+return(
+
+
+    <div>
+
+        <h1>Esercizio Numero 1</h1> <h3><b>Mostra e Nascondi Bottone</b></h3>
+
+
+        <br></br>
+
+
+        <button onClick={mostraContenuto()}>
+
+           {mostraTesto ?  "Nascondi" : "Mostra"}
+        </button>
+
+        {mostraTesto && <p>Primo esercizio BASE SU REACT !</p>}
+
+    </div>
+
+
+)
+}
+ export default NascondiMostra;
