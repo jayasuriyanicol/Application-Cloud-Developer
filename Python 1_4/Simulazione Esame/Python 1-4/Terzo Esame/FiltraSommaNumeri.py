d15e3e98b980f980a1275db0d22d16a3f548d47c
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2025-11-02 20:30:46 +0100

    UPDATE - 02_11_2025 | Update comment on FiltraSommaNumeri

--- Python 1_4/Simulazione Esame/Python 1-4/Terzo Esame/FiltraSommaNumeri.py
+++ Python 1_4/Simulazione Esame/Python 1-4/Terzo Esame/FiltraSommaNumeri.py
@@ -1,5 +1,17 @@
+'''
+Filtra e Somma Numeri – PUNTI 1
 
+Scrivi una funzione con il seguente header:
+filter_and_sum(nums: list[int], min_val: int) -> int
 
+La funzione deve prendere una lista di numeri interi e sommare tutti i numeri maggiori di min_val.
+Restituisci la somma risultante.
+
+📘 Esempio:
+
+filter_and_sum([2, 7, 3, 10, 5], 4) ➜ 22
+
+'''
 
 def filter_and_sum(nums:list[int],min_val:int) -> int:
 
