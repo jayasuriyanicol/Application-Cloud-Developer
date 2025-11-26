87a0da9c47460c5ef2d264ca3290397dbff9b844
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2025-11-26 12:13:43 +0100

    UPDATE - 26_11_2025 | Updating the Sommatoria Exercise adding the variable potenza  avoid the Math Library

--- Java 1_6/Basi di Java/Esercizi Introduttivi/2_0 Lezione.java
+++ Java 1_6/Basi di Java/Esercizi Introduttivi/2_0 Lezione.java
@@ -4,42 +4,38 @@
 
 package corsoBase;
 import java.util.Scanner;
+
 public class Esercizio2_0 {
-	public static void main(String[] args) {
-		
-		int n =  0;
-		int k =  0;
-		int risultato = 0;
-		
-		        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        
+        int n = 0;
+        int k = 0;
+        int potenza = 1; 
+        int risultato = 0;
+        
+        Scanner scanner = new Scanner(System.in);
 
-		        System.out.println("Inserisci il primo numero (n):");
-		        n = scanner.nextInt();
+        System.out.println("Inserisci l'esponente massimo (n):");
+        n = scanner.nextInt();
 
-		        System.out.println("Inserisci il secondo numero (k):");
-		        k = scanner.nextInt();
-		        
-		        scanner.close();
-
-		        risultato = calcolaSommatoria(n, k);
-
+        System.out.println("Inserisci la base (k):");
+        k = scanner.nextInt();
+        
+        scanner.close();
+        
+        
+        for (int i = 1; i <= n; i++) {
+            
+            
+            potenza *= k;
+            
+            risultato += potenza; 
+        }
+        
+     
 		        System.out.println("La sommatoria dei numeri tra " + n + " e " + k + " (inclusi) è: " + risultato);
 		    }
 
 
 
-
-public static int calcolaSommatoria(int n, int k) {
-    int somma = 0;
-   
-    int min = Math.min(n, k);
-    int max = Math.max(n, k);
-
-
-    for (int i = min; i <= max; i++) {
-        somma += i; 
-    }
-
-    return somma;
-}
 }
