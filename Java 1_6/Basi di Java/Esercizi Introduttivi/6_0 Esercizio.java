541a1a73a6d5965baaaede32b283bcfc947cdc5f
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2025-11-26 12:14:18 +0100

    UPDATE - 26_11_2025 | Updating the Numero Primo Exercise adding new fix

--- Java 1_6/Basi di Java/Esercizi Introduttivi/6_0 Esercizio.java
+++ Java 1_6/Basi di Java/Esercizi Introduttivi/6_0 Esercizio.java
@@ -12,45 +12,62 @@ import java.util.Scanner;
 
 public class Esercizio6_0 {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        int n;
 
-		
-		Scanner scanner = new Scanner(System.in);
-		int n;
-		
-		
-		System.out.println("Inserisci un numero: ");
-		
-		do {
-			n = scanner.nextInt();
-			
-			if (n <= 0) {
-				
-				System.out.println("Inserire un numero > 0");
-			}
-			
-		} while (n <= 0);
-		
-		System.out.println("Hai inserito\t" + n + "\n");
-		
-		System.out.print("I fattori di " + n + ": ");
-		
-		boolean first = true;
-		
-		while (n > 1) {
-			for (int c = 2; c <= n; c++) {
-				if (n % c == 0) {
-					if (first) {
-						System.out.print(c);
-						first=false;
-					} else {
-						System.out.print("*" + c);
-					}
-					n /= c;
-					break;
-				}
-			}
-		}
-	}
+        System.out.println("Inserisci un numero: ");
+
+        do {
+            n = scanner.nextInt();
+
+            if (n <= 0) {
+                System.out.println("Inserire un numero > 0");
+            }
+
+        } while (n <= 0);
+        
+        //I used this form to compare the n with him self, if is true is a FIRST NUMBER
+        int numeroInserito = n;
+
+        System.out.println("\nHai inserito " + n + ": ");
+
+        //Special Case, because  1 <= n <= 2 is a FIRST NUMBER
+        
+        if (n== 1 || n == 2) {
+            System.out.println("\nIl numero " + n + " è un numero primo");
+        } else {
+         
+            while (n > 1) {
+                for (int cont = 2; cont <= n; cont++) {
+                    
+                    
+                    if (n % cont == 0) {
+
+                        if (cont == numeroInserito) {
+                            System.out.println("\nIl numero " + numeroInserito + " è un numero primo");
+                            n = 1; 
+                            break; 
+                        }
+
+                        
+                        System.out.print(cont);
+                        
+                      
+                        n /= cont;
+
+                        
+                        if (n > 1) {
+                            System.out.print(" * ");
+                        }
+                        
+                     
+                        break; 
+                    }
+                }
+            }
+        }
+        scanner.close();
+    }
 }
