92ab697f3c3cf195edd6732a5ed9821376a2322d
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2025-12-03 13:57:30 +0100

    UPDATE  - 03_12_2025 | Updating PostGrid adding solution of teacher and username

--- WEB/WEB 2/vite-cloud/src/PostGrid.jsx
+++ WEB/WEB 2/vite-cloud/src/PostGrid.jsx
@@ -18,7 +18,7 @@ const PostGrid = () => {
 
             const userP=fetch(URL_USERS+post.userId).then(reso => resp.json())
             const commentsP=fetch(URL_USERS+"?postId="+post.id).then(reso => resp.json())
-            const[user,commenti] = Promise.all([userP, commentsP] ) 
+            const[user,commenti] = await Promise.all([userP, commentsP] ) 
             return{
 
                 ...post,
@@ -47,16 +47,71 @@ const PostGrid = () => {
     
    },[]);
 
-
   return (
-
     <div>
-
-       {posts.map(p =>(<p>{p.title}</p>))}
-
-
+      {posts.map((p) => (
+        <div><p>{p.title}</p>
+         <p>Nome {p.user}</p>
+         </div>
+      ))}
     </div>
   );
 };
 
 export default PostGrid;
+
+
+
+
+{ /* Import React, { useEffect, useState } from "react";
+
+const PostGrid = () => {
+  const URL_POSTS = "https://jsonplaceholder.typicode.com/posts";
+  const URL_USERS = "https://jsonplaceholder.typicode.com/users/";
+  const URL_COMMENTS = "https://jsonplaceholder.typicode.com/comments";
+  const [posts, setPosts] = useState([]);
+
+async function fetchPostDetails(post) {
+  try {
+    const userPromise = fetch(URL_USERS + post.userId).then(resp => resp.json());
+    const commentsPromise = fetch(URL_COMMENTS + "?postId=" + post.id).then(resp => resp.json());
+
+    const [user, comments] = await Promise.all([userPromise, commentsPromise]);
+
+    return {
+      ...post,
+      user: user.name, 
+     
+      comments: comments//.slice(0, 5) 
+    };
+  } catch (error) {
+    console.error(`Errore nel caricamento dettagli post ${post.id}:`, error);
+    return { ...post, user: 'Sconosciuto', comments: [] };
+  }
+}
+  useEffect(() => {
+    fetch(URL_POSTS)
+      .then((response) => response.json())
+      .then((allPosts) => {
+
+        const postsArricchitiPromise=allPosts.map(fetchPostDetails)
+        console.log(postsArricchitiPromise)
+        Promise.all(postsArricchitiPromise).then(allPostsArricchiti=>setPosts(allPostsArricchiti))
+        
+
+
+      });
+  }, []);
+  return (
+    <div>
+      {posts.map((p) => (
+        <div><p>{p.title}</p>
+         <p>Nome {p.user}</p>
+         </div>
+      ))}
+    </div>
+  );
+};
+
+export default PostGrid;
+y*/}
