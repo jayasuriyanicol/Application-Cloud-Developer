c629c06d50d66d717b752caaa2f47e350a3e94de
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2025-12-11 18:09:31 +0100

    UPDATE - 11_12_2025 | Updating adding another example usign a class comparator for 'Impiegato'

--- Java 1_7/Basi di Java/Lezioni Java/Lezione_08.java
+++ Java 1_7/Basi di Java/Lezioni Java/Lezione_08.java
@@ -60,6 +60,52 @@ public class Lezione8_0 {
 
     */
 	
+	
+	
+	/*
+	 Other type of example, list of Impiegati
+	 
+	 ArrayList<Impiegato> listaImp = new ArrayList<>();
+	 
+	 listImp.add(new Impiegato("Mario", 1500, new Date());
+	 listImp.add(new Impiegato("Anna",  1350, new Date(120,1,1));
+	 listImp.add(new Impiegato("Mario", 1525, new Date(110,5,30));
+	 listImp.add(new Impiegato("Manuel",1300, new Date(125,1,1));
+	 
+	 //Error : it can execute because is not comparable. So, going to class Impiegato and add comparable in the class and do the override.
+	 //Collections.sort(listaImp);
+	 
+	 //After we manage the situation, we can continue and print it 
+	 for (Impiegato impiegato : listaImp) {
+	 
+	 	System.out.println(impiegato);
+	 
+	 }
+	 
+	 /*CLASS IMPIEGATO:
+	  
+	   public class Impiegato implements Comparable
+	   
+		   @Override
+		   public int compareTo(Impiegato o) {
+		   	
+		   	//Criterio, order by sort asc name
+		   	return this.nome.compareTo(o.nome);
+	
+	   
+	   	return 0;
+	   	}
+	   	
+	  	//Finally, we have to rember if a specific method is used more than once a time, we need to create a specific class for the function and invocate it.
+	  	//Basically, before start coding is a good behaviour to check if there are some method to create.
+	  	
+	  				//Syntax ->  Collections.sort(listArray, comparator)
+	  	
+	  	 */
+	 
+	 
+
+	
 
 	
 	
