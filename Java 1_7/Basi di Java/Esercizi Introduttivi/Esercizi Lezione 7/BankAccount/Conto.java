cb8979ffa4f84ce329dd20185d51800c5defc64f
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2025-12-11 11:49:09 +0100

    ADD - 11_12_2025 | Adding the Conto of exercise BankAccount

---
+++ Java 1_7/Basi di Java/Esercizi Introduttivi/Esercizi Lezione 7/BankAccount/Conto.java
@@ -0,0 +1,32 @@
+/*
+ Classe Conto
+ 
+ Un conto standard è caratterizzato da un intestatario e un numero.
+	Deve memorizzare anche il saldo e la lista dei movimenti.
+	Prevedere 2 costruttori:
+	Con deposito iniziale
+	Senza deposito iniziale
+	Sul conto si possono eseguire prelievi e versamenti, ma sarà fornito un solo metodo che riceve e gestisce un movimento (Movement)
+	Il prelievo è fattibile solo se l’ammontare non supera il saldo
+	Inoltre si richiede una funzione di verifica coerenza (checkBalance), la quale controlla se il saldo del conto coincide con la somma algebrica dei movimenti
+	Si prevede infine un metodo per stampare i dati del conto ed un altro per stampare solo la lista dei movimenti.
+
+ */
+
+
+package eserciziLezione7;
+
+public class Conto {
+   
+	//In this case we create a mother class, that have SAVING and STANDARD account bank as subclasses
+	
+	
+	
+	
+
+	
+	
+	
+}
+
+
