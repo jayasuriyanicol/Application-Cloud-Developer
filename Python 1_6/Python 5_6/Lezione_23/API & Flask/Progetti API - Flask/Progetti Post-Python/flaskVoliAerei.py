b137cc20c9a71602416be40b30f95e43c4fe88ae
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2025-12-30 19:48:32 +0100

    UPDATE - 30_12_2025 | Fixing and sub categorizing the folders in order to levels

--- Python 1_6/Python 5_6/Lezione_23/API & Flask/Progetti API - Flask/Progetti Post-Python/flaskVoliAerei.py
+++ Python 1_6/Python 5_6/Lezione_23/API & Flask/Progetti API - Flask/Progetti Post-Python/flaskVoliAerei.py
@@ -13,23 +13,24 @@ def AboutVoli() -> str:
 
 #Metodo GET per ottenere i numero dei voli totali presenti nel 'DB'
 @voli.route('/voli',methods =['GET'])
-
 def get_voli():
 
-    return jsonify({'Totali Voli': len(dati)})
-
-@voli.route('/Insiemevoli',methods=['GET'] )
+    return jsonify({'Totali Voli': len(dati['Volo'])})
 
 
 #Metodo GET per ottenere tutti i voli presenti nel 'DB'
+@voli.route('/Insiemevoli',methods=['GET'] )
 def get_InsiemeVoli():
+
     return jsonify({'Voli': dati['Volo']})
 
 
-
+#Metodo GET per ottenere tutte le nazioni presenti nel 'DB'
 @voli.route('/nazioni',methods=['GET'] ) 
+def get_nazioni_all():
 
-      return jsonify({'Nazioni' : dati['Nazioni'] })
+      return jsonify({'Nazioni' : dati['Nazione'] })
+
 
 #Metodo GET per ottenere tutte le NAZIONI presenti nel 'DB'
 @voli.route('/Insiemenazioni',methods=['GET'] )
@@ -38,25 +39,19 @@ def get_nazioni():
         return jsonify({'Nazioni': dati['Nazione']})
 
 
-
-
-
 #Metodo POST per inserire una nuova nazione previa verifica se non esiste già nel 'DB'
 @voli.route('/nazioni', methods=['POST'])
-
 def add_nazione():
 
      nuovo = request.get_json()
 
      if not nuovo or 'nome' not in nuovo:
 
-            return{'errore' : 'Output non valido ai fini di risposta, prego inserire CORRETTAMENTE I DATI '},400
+            return jsonify({'errore' : 'Output non valido ai fini di risposta, prego inserire CORRETTAMENTE I DATI '}),400
      
      nuovoId = len(dati['Nazione']) + 1
 
      nuovaNazione = {
-
-
           'id': nuovoId, 
           'nome': nuovo['nome'],
          }
@@ -66,17 +61,15 @@ def add_nazione():
      return jsonify(nuovaNazione),201
 
 
-
-
 #Gestione delgi ErrorHandler personalizzato 
 @voli.errorhandler(400)
-
 def requiredName(error):
         return jsonify({
               
               'ERRORE': 'ATTENZIONE ! il campo del testo va compilato',
               'CODICE ERRORE': 400
         }),400
+
 
 @voli.errorhandler(404)
 def notFound(error):
@@ -86,19 +79,8 @@ def notFound(error):
               'NOT FOUND' : 'VALORE NON TROVATO',
               'CODICE ERRORE' : 404
               
-
         }),404
 
 
-
-
-
-
-
-
-
-
-
-
-
- 
+if __name__ == '__main__':
+    voli.run(debug=True)
