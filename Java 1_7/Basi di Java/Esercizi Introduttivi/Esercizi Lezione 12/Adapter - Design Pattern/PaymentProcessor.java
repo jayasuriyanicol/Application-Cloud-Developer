3e32556c9904723da38cb9c8dbd4848e27e36066
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2026-01-12 13:49:07 +0100

    ADD - 12_01_2026 | Adding the class PaymentProcessor of Exercise  'Adapter DesignPattern'

---
+++ Java 1_7/Basi di Java/Esercizi Introduttivi/Esercizi Lezione 12/Adapter - Design Pattern/PaymentProcessor.java
@@ -0,0 +1,14 @@
+'''
+Si consideri un sistema software per la gestione dei pagamenti che utilizza la seguente interfaccia:
+
+public interface PaymentProcessor {
+    void pay(double amount);
+}
+'''
+package eserciziLezione12;
+
+public interface PaymentProcessor {
+	
+	void pay (double amount);
+
+}
