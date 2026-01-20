b040c8d4f4f03cf9aef4fe2db31670294697a263
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2026-01-20 11:00:30 +0100

    ADD - 20_01_2026 | Adding the  Exception  ->  'PostiNonDispException'  of 'PrenotaVoli Thread' exercise

---
+++ Java 1_7/Basi di Java/Esercizi Introduttivi/Esercizi Lezione 13/PrenotaVoli/PostiNonDispException.java
@@ -0,0 +1,20 @@
+package eserciziLezione13;
+
+@SuppressWarnings("serial")
+public class PostiNonDispException extends Exception {
+	
+	public PostiNonDispException() {
+		
+		super();
+	}
+
+
+public PostiNonDispException(String motivazione) {
+	
+	super(motivazione);
+}
+	
+	
+}
+
+
