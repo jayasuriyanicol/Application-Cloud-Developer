d9869ea3e6161a7aca2e69832bc7fde73d9f2c53
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2026-02-01 19:29:38 +0100

    ADD - 01_02_2026 | Adding correctly position old files of folder 'Lezioni Teoria'

---
+++ Java /Java 1_3/Java 1/Argomenti/Classi/Progetti/Esercizi Stringhe/ScannerIntNumber.java
@@ -0,0 +1,21 @@
+package corsoBase;
+
+import java.util.Scanner;
+//Example of using the System.out giving a data and printing it
+
+public class Lezione2_0 {
+
+	public static void main(String[] args) {
+		System.out.println();
+		
+		char carattere = '\\';
+		String s = "r";
+		boolean b = false;
+
+System.out.println("Inserisci un numero intero: ");
+Scanner sc = new Scanner(System.in);
+int n = sc.nextInt();
+System.out.println("Hai inserito il seguente numero:" + n);
+}
+
+}
