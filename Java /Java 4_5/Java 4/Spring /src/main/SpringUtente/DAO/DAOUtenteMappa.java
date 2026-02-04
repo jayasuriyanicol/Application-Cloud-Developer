0a82f78a52f3522b1611e85b0bbec7091ddb84e2
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2026-02-04 11:27:22 +0100

    ADD - 04_02_2026 | Adding ' DAOUtenteMappa' of project SpringUtenteApplication, creation the Data Accsss Object path of Application

---
+++ Java /Java 4_5/Java 4/Spring /src/main/SpringUtente/DAO/DAOUtenteMappa.java
@@ -0,0 +1,81 @@
+package DAO;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.spring.utenti.entity.Utente;
+
+
+
+/* * DAOUtenteMappa - In-Memory Data Access Object
+    ? Implements the persistence layer using a Java `HashMap` to simulate a database. It stores `Utente` entities in volatile memory, acting as a mock repository for rapid testing or prototyping without SQL overhead.
+
+    ! 1. insert(Utente utente), manages data entry by checking for key existence (ID collision) before committing the object to the internal map.
+    ! 2. cercaPerNome(String nomeDaCercare), performs a manual linear search over the collection values to filter results, handling case-insensitive string matching.
+    ! 3. selectAll() / selectById(), provides standard retrieval mechanisms by extracting values directly from the key-value store.
+*/
+
+
+public class DAOUtenteMappa {
+
+	private Map<Integer, Utente> mappa = new HashMap<>();
+
+	public boolean insert(Utente utente) {
+		if(mappa.containsKey(utente.getIdUtente()))
+			return false;
+		
+		mappa.put(utente.getIdUtente(), utente);
+		return true;
+
+	}
+	public List<Utente> selectAll(){
+		return new ArrayList<>(mappa.values());
+	}
+
+	public Utente selectById(Integer idUtente) {
+		return mappa.get(idUtente);
+	}
+	
+	public Utente delete(Integer idUtente) {
+		Utente utente = mappa.remove(idUtente);
+		return utente;
+	}
+	
+	
+	public int numeroUtenti() {
+		
+		return mappa.size();
+	}
+	
+	
+	public List<String> getNomiUtenti() {
+		
+        List<String> nomi = new ArrayList<>();
+        
+        for (Utente u : mappa.values()) {
+            nomi.add(u.getNome());
+        }
+        return nomi;
+    }
+
+    
+    public List<Utente> cercaPerNome(String nomeDaCercare) {
+    	
+        List<Utente> risultati = new ArrayList<>();
+        
+        for (Utente u : mappa.values()) {
+            
+            //In this case, we add a method to ignore the case sensitive problem (Lower and Upper cases)
+            if (u.getNome().equalsIgnoreCase(nomeDaCercare)) {
+                risultati.add(u);
+            }
+        }
+        return risultati;
+    }
+		
+
+}
+	
+
