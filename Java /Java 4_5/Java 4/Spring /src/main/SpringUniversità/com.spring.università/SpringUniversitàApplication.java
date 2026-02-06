a69e7fd2bbe042d271a51dacdddcc9165ee66af0
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2026-02-06 19:20:36 +0100

    ADD - 06_02_2026 | Adding the main run app of 'SpringUniverisitàApplication' of Progetto Spring Università'

---
+++ Java /Java 4_5/Java 4/Spring /src/main/SpringUniversità/com.spring.università/SpringUniversitàApplication.java
@@ -0,0 +1,21 @@
+package com.spring.università;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/* * SpringUniversitaApplication - Application Bootstrapper
+    ? The main entry point for the University management Spring Boot application, initializing the Spring container and triggering auto-configuration.
+
+    ! 1. @SpringBootApplication, a meta-annotation that combines @Configuration, @EnableAutoConfiguration, and @ComponentScan to configure the application context automatically.
+    ! 2. main(String[] args), launches the application by passing this class to the SpringApplication.run() helper method.
+*/
+
+@SpringBootApplication
+public class SpringUniversitaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringUniversitaApplication.class, args);
+	}
+
+}
