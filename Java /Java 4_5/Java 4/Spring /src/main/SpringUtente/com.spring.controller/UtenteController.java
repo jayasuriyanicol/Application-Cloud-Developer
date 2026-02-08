04ba2b2dc6cda7b3b1b5f1d5f41b3c02bf067ef5
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2026-02-08 15:44:06 +0100

    UPDATE - 08_02_2026 | Fixing the '@PathVariable' and minor fix to launch correctly Spring

--- Java /Java 4_5/Java 4/Spring /src/main/SpringUtente/com.spring.controller/UtenteController.java
+++ Java /Java 4_5/Java 4/Spring /src/main/SpringUtente/com.spring.controller/UtenteController.java
@@ -25,81 +25,72 @@ import com.spring.utenti.service.UtenteService;
 
 
 //Method used to initialize the web request handling components or bean after all bean properties have been set.
-@RestController 
-@RequestMapping(path="/utenti")
+package com.spring.controller;
+
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.spring.dto.UtenteDTO;
+import com.spring.entity.Utente;
+import com.spring.service.UtenteService;
+
+
+@RestController
+@RequestMapping(path = "/utenti")
 public class UtenteController {
-	
-	private UtenteService service  = new UtenteService();
-	
-	@GetMapping(path="/salva", consumes= "application/json")
-	
-	public boolean salva(@RequestBody Utente utente) {
-		
+	private UtenteService service = new UtenteService();
+
+	@GetMapping(path = "/salva", consumes = "application/json")
+	public boolean salva(@RequestBody UtenteDTO utente) {
 		return service.registra(utente);
-		
 	}
-	
 
-
-
-/* * Spring Annotations Reference
-    ? Technical breakdown of the metadata used to configure the web layer behavior.
-
-    ! 1. @RestController -> marks this class as a web controller where every method inherits @ResponseBody. This means return values are written directly to the HTTP response body (usually as JSON) rather than resolving to a templated view (HTML).
-    ! 2. @RequestMapping(path="/utenti") -> establishes the base URI prefix for the entire class. All endpoints defined inside will be relative to "localhost:8080/utenti".
-    ! 3. @GetMappin ->  a specialized shortcut for @RequestMapping(method = RequestMethod.GET). It instructs Spring to route only HTTP GET requests matching the specified path to the annotated method.
-*/
-
-
-	
-	@GetMapping(path="/cerca/{idUtente}", produces = "application/json")
-	public Utente visualizza(@PathVariable int idUtente) {
-		
+	@GetMapping(path = "/cerca/{idUtente}", produces = "application/json")
+	public UtenteDTO visualizza(@PathVariable int idUtente) {
 		return service.cercaPerId(idUtente);
 	}
-	
-	
-	@GetMapping(path="/lista", produces = "application/json")
-	public ArrayList<Utente> lista() {
-		
-		return service.selectAll();
+
+	@GetMapping(path = "/cancella/{idUtente}", produces = "application/json")
+	public Utente cancellaUtente(@PathVariable int idUtente) {
+		return service.eliminaUtente(idUtente);
 	}
-	
-	
-	@GetMapping(path="/cancella/{idUtente}", produces ="application/json")
-	public Utente cancella(@PathVariable int idUtente) {
-		
-		return service.cancellaUtente(idUtente);
+
+	@GetMapping(path = "/listaCompleta", produces = "application/json")
+	public List<Utente> listaCompletaUtenti() {
+		return service.listaUtenti();
 	}
-	
-	
-	@GetMapping(path="/emailAggiorna/{idUtente}", produces ="application/json")
-	public Utente emailAggiorna(@PathVariable int idUtente, String mail) {
-		
-		return service.modificaMail(idUtente, mail);
+
+	@GetMapping(path = "/aggiornaEmail/{idUtente}/{mail}", produces = "application/json")
+	public Utente changeEmail(@PathVariable int idUtente,@PathVariable String mail) {
+		return service.aggiornaEmail(idUtente, mail);
 	}
-	
-	
-	
-	@GetMapping(path="/numeroUtenti", produces="application/json")
-	public int numeroUtenti() {
-		
-		return service.numeroUtenti();
+
+	@GetMapping(path = "/numeroUtenti")
+	public int getNumeroUtenti() {
+		return service.getNumeroUtenti();
 	}
-	
-	
-	@GetMapping(path="/getNomiUtenti", produces="application/json")
-	public List<String> getNomiUtenti() {
-		
+
+	@GetMapping(path = "/listaNomi")
+	public List<String> getListaNomi() {
 		return service.getNomiUtenti();
 	}
-	
-	@GetMapping(path="/cercaPerNome/{nomeDaCercare}", produces="application/json")
-    public List<Utente> cercaPerNome(@PathVariable String nomeDaCercare) {
-    	
-        
-        return service.cercaPerNome(nomeDaCercare);
-    }
+
+	@GetMapping(path = "/cercaPerNome/{nome}", produces = "application/json")
+	public List<Utente> cercaUtentiPerNome(@PathVariable String nome) {
+		return service.cercaPerNome(nome);
+	}
+
+	@GetMapping(path = "/nomiNumero", produces = "application/json")
+	public int getNomiNumeroUtenti() {
+		return service.getNumeroUtenti();
+	}
+}
 		
 	
 	
