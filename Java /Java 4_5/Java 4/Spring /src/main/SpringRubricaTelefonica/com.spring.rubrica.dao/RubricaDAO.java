92dc4a5dccc56c2d4f50d9f27d748548e0c313b1
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2026-02-13 13:52:32 +0100

    UPDATE - 13_02_2026 | Updating fixing the problems adding also excpetion cases

--- Java /Java 4_5/Java 4/Spring /src/main/SpringRubricaTelefonica/com.spring.rubrica.dao/RubricaDAO.java
+++ Java /Java 4_5/Java 4/Spring /src/main/SpringRubricaTelefonica/com.spring.rubrica.dao/RubricaDAO.java
@@ -1,8 +1,12 @@
 package com.spring.rubrica.dao;
 
-	import java.util.*;
-	import org.springframework.stereotype.Repository;
-	import com.spring.rubrica.entity.rubrica;
+import java.util.*;
+import org.springframework.stereotype.Repository;
+import com.spring.rubrica.interceptor.ExcpetionNotFoundID;
+import com.spring.rubrica.entity.ContattoTelefonico;
+import com.spring.rubrica.entity.Rubrica;
+
+
 
 /* * RubricaDAO - In-Memory Persistence Layer
     ? Serves as the data access component for the application, simulating a database using a Java Collection. It manages the lifecycle of 'rubrica' objects directly in memory without a persistent backing store.
@@ -13,32 +17,72 @@ package com.spring.rubrica.dao;
 */
 
 
+
 @Repository
 public class RubricaDAO {
 
 		//We create the map to simulate the Database (searching the ID)-> Object Rubrica
-	    private Map<Integer, rubrica> mappa = new HashMap<>();
+	    private Map<Integer, Rubrica> mappa = new HashMap<>();
 	    private int id = 1;
 
-	    public rubrica insert(rubrica r) {
+	    
+	    
+	    //Insertion of the error of duplicated ID, setted to void
+	    public void insert(Rubrica r) {
+	    	
+	    	if(mappa.containsKey(r.getId()))
+	    		
+	    		throw new RuntimeException("ERRORE: Non è possibile creare due volte lo stesso utente con lo stesso ID !");
 	        r.setId(id++);
 	        mappa.put(r.getId(), r);
-	        return r;
+	      
+	    }
+	    
+	    
+	    
+	    //Insertion of the error of duplicated ID, setted to void
+	    public void insertContact(int idRubrica, ContattoTelefonico c) {
+
+	    	    if (!mappa.containsKey(idRubrica))
+	    	    	
+	    	        throw new RuntimeException("ATTENZIONE: Non è stato possibile individuare la rubrica di appartenza: RUBRICA NON ESISTENTE !");
+
+	    	    Rubrica r = mappa.get(idRubrica);
+
+	    	    boolean inserito = r.getContatti().add(c);
+
+	    	    if (!inserito)
+	    	        throw new RuntimeException("ATTENZIONE: Contatto già presente NOME e COGNOME duplicati");
+	    	}
+
+	
+	        
+	      
+	   
+
+	  
+
+	    public Rubrica getById(int id) {
+	    	
+	    	if(mappa.containsKey(id))
+	    		
+	    		
+	    		return mappa.get(id);
+
+	   
+	    	
+	    	throw new ExcpetionNotFoundID("ERRORE: Attenzione non è possibile proseguire con la richiesta, ID non esistente");
 	    }
 
-	    public rubrica getById(int id) {
-	        return mappa.get(id);
-	    }
-
-	    public List<rubrica> getAll() {
+	    public List<Rubrica> getAll() {
 	        return new ArrayList<>(mappa.values());
 	    }
 
-	    public rubrica delete(int id) {
+	    public Rubrica delete(int id) {
 	        return mappa.remove(id);
 	    }
 
-	    public Map<Integer, rubrica> getMappa() {
+	    public Map<Integer, Rubrica> getMappa() {
 	        return mappa;
 	    }
 	}
