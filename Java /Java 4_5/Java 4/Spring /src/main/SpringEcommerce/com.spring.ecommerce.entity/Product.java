f77d9dd5be50b811bf7609a84c8eabcf60511b1c
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2026-02-15 19:12:12 +0100

    ADD - 15_02_2026 | Adding  ENTITIES & Status of SpringEcommerce exercise Spring REST

---
+++ Java /Java 4_5/Java 4/Spring /src/main/SpringEcommerce/com.spring.ecommerce.entity/Product.java
@@ -0,0 +1,69 @@
+package com.spring.ecommerce.entity;
+
+public class Product {
+	
+	private int id;
+	private String name,stock;
+	private double price;
+	
+	
+	public Product(int id, String name, String stock, double price) {
+		
+		this.id = id;
+		this.name = name;
+		this.stock = stock;
+		this.price = price;
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getStock() {
+		return stock;
+	}
+
+
+	public void setStock(String stock) {
+		this.stock = stock;
+	}
+
+
+	public double getPrice() {
+		return price;
+	}
+
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+
+	@Override
+	public String toString() {
+		return "|PRODOTTO|\n  ID -> " + id + "\nNOME -> " + name + "\nSTOCK -> " + stock + "\nPREZZO ->" + price ;
+	}
+	
+	
+	
+	
+	
+
+}
---
+++ Java /Java 4_5/Java 4/Spring /src/main/SpringEcommerce/com.spring.ecommerce.entity/Status.java
@@ -0,0 +1,14 @@
+package com.spring.ecommerce.entity;
+
+
+//Creation of a class STATUS to manipulate more simply the imports and creation of the status odf orders
+public enum Status {
+	
+		CREATED,
+		CONFIRMED,
+		SHIPPED,
+		DELIVERED,
+		CANCELLED
+	}
+
+
