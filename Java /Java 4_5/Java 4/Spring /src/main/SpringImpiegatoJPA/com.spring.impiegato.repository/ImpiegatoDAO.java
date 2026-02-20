80e400cfb832f42b04cc1cef101e61dc44447b4e
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2026-02-20 15:27:21 +0100

    Updating the REPOSITORY | JPA DAO  with the new Query Native PostGreSQL

--- Java /Java 4_5/Java 4/Spring /src/main/SpringImpiegatoJPA/com.spring.impiegato.repository/ImpiegatoDAO.java
+++ Java /Java 4_5/Java 4/Spring /src/main/SpringImpiegatoJPA/com.spring.impiegato.repository/ImpiegatoDAO.java
@@ -1,7 +1,15 @@
 package com.spring.impiegati.repository;
 
+
 import com.spring.impiegati.entity.Impiegato;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 
 /* * ImpiegatoDAO - HR Persistence Gateway
     ? A Spring Data JPA repository interface that acts as the automated data access layer for the Employee module. By extending JpaRepository, it provides a sophisticated abstraction over the database, allowing for complex HR data management without the need for manual SQL implementation.
@@ -17,5 +25,28 @@ public interface ImpiegatoDAO extends JpaRepository<Impiegato,Integer> {
 	
 	
 	//Only EREDITO the methods of JPARepository
+	
+	
+	
+	//For doing the advanced method we adding some QUERY NATIVES 
+	
+	
+	@Query(nativeQuery = true, value = "SELECT * FROM impiegato ORDER BY cognome")
+	public List<Impiegato> ImpiegatoOrdCognome();
+	
+	@Query(nativeQuery = true, value="SELECT * FROM impiegato ORDER BY matricola")
+	public List<Impiegato> ImpiegatoOrdMatricola();
+	
+	
+	//To avoid incongruence I add a default value 0 to prevent any Excpetion during launch of the APP
+	@Query(nativeQuery = true, value="SELECT COALESCE(SUM(i.salario_mensile),0) FROM impiegato i")
+	public Double totaleSalario();
+	
+	
+
+	@Query("SELECT i FROM Impiegato i  WHERE i.dataAssunzione > :dataSpecificata ORDER BY i.salarioMensile DESC")
+	public List<Impiegato> salarioMaxDatSPec(@Param("dataSpecificata") LocalDateTime dataSpecificata);
+	
+	
 	
 }
