06ac85d2177061df6d3eb7291b819a9dce703717
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2026-02-23 20:00:05 +0100

    Add SpringAziendaJpaApplication class for Spring Boot application setup Exercie Azienda JPA

---
+++ Java /Java 6_7/Java 6/SpringJPAHibernate/src/main/SpringAziendaJPA/com.spring.azienda/SpringAziendaJpaApplication.java
@@ -0,0 +1,13 @@
+package com.spring.azienda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAziendaJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAziendaJpaApplication.class, args);
+	}
+
+}
