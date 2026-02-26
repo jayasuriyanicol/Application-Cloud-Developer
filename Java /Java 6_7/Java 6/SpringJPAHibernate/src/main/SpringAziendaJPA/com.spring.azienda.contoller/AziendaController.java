ea484672d8ec0b381c0f961ea95c234efd089ec1
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2026-02-26 21:08:52 +0100

    Fixing the Controller AziendaJPA
    Fixing the controller method to adapt to the exercise adding the missed one's and check all the methods correctly by PostMan

--- Java /Java 6_7/Java 6/SpringJPAHibernate/src/main/SpringAziendaJPA/com.spring.azienda.contoller/AziendaController.java
+++ Java /Java 6_7/Java 6/SpringJPAHibernate/src/main/SpringAziendaJPA/com.spring.azienda.contoller/AziendaController.java
@@ -15,24 +15,50 @@ public class AziendaController {
     @Autowired
     private AziendaService aziendaService;
 
+    
 
     @PostMapping(path = "/inserisciAzienda", consumes = "application/json", produces = "application/json")
     public void inserisciNuovaAzienda(@RequestBody AziendaDTO aziendaDTO) {
         aziendaService.inserisciNuovaAzienda(aziendaDTO);
     }
-
+    
+    @GetMapping(path = "/mostraAziId/{idAzienda}", produces = "application/json")
+    public AziendaDTO visualizzaDatiCompleti(@PathVariable Integer idAzienda) {
+        return aziendaService.visualizzaDatiAzienda(idAzienda);
+    }
+    
+    @GetMapping(path = "/mostraAziBaseId/{idAzienda}", produces = "application/json")
+    public AziendaDTO visualizzaDatiBase(@PathVariable Integer idAzienda) {
+        return aziendaService.visualizzaDatiBase(idAzienda);
+    }
     
     @GetMapping(path= "/mostraAziende", produces = "application/json")
     public List<AziendaDTO> visualizzaTutteAziende() {
         return aziendaService.visualizzaTutteAziende();
     }
-
+  
+    
+    @GetMapping(path = "/mostraAziNomDip/{idAzienda}", produces = "application/json")
+    public AziendaInfoDTO visualizzaNomeNumDip(@PathVariable Integer idAzienda) {
+        return aziendaService.visualizzaNomeNumDip(idAzienda); 
+    }
     
     @GetMapping(path = "/mostraAziCapMax", produces = "application/json")
-    public AziendaInfoDTO visualizzaNomeNumDip() {
-        return aziendaService.visualizzaNomeNumDip(null); 
+    public AziendaInfoDTO visualizzaAziendaTopCapitale() {
+        return aziendaService.visualizzaMaggioreCapitaleAzienda();
+    }
+    
+ 
+    @PutMapping(path = "/modificaCapAziId/{idAzienda}/{nuovoCapitale}", produces = "application/json")
+    public AziendaDTO modificaCapitale(@PathVariable Integer idAzienda,  @PathVariable Double nuovoCapitale) {
+        return aziendaService.modificaCapitaleAzienda(idAzienda, nuovoCapitale);
     }
 
+    
+    @PutMapping(path = "/modificaIntestAziId/{idAzienda}/{nuovaIntestazione}", produces = "application/json")
+    public AziendaDTO modificaIntestazione( @PathVariable Integer idAzienda, @PathVariable String nuovaIntestazione) {
+        return aziendaService.modificaIntestazioneAzienda(idAzienda, nuovaIntestazione);
+    }
     
     @DeleteMapping(path = "/cancellaAzi/{idAzienda}", produces = "application/json")
     public AziendaDTO cancellaAziendaNoDipendenti(@PathVariable Integer idAzienda) {
--- Java /Java 6_7/Java 6/SpringJPAHibernate/src/main/SpringAziendaJPA/com.spring.azienda.contoller/DipendenteController.java
+++ Java /Java 6_7/Java 6/SpringJPAHibernate/src/main/SpringAziendaJPA/com.spring.azienda.contoller/DipendenteController.java
@@ -14,18 +14,32 @@ public class DipendenteController {
 	    private DipendenteService dipendenteService;
 
 	   
-	    @PostMapping(path = "/nuovoDipendente/{idAzienda}/{idPosto}", consumes = "application/json", produces = "application/json")
-	    public void inserisciDipendente (@RequestBody DipendenteDTO dto,@PathVariable Integer idAzienda, @PathVariable Integer idPosto) {
+	    @PostMapping("/nuovoSenzaPosto/{idAzienda}")
+	    public void inserisciSenzaPosto(@PathVariable Integer idAzienda, @RequestBody DipendenteDTO dto) {
+	        dipendenteService.inserisciDipendenteSenzaPostoAuto(idAzienda, dto);
+	    }
+	    
+	    @PostMapping(path = {"/nuovoDipendente/{idAzienda}/{idPosto}", "/nuovoDipendente/{idAzienda}"})
+	    public void inserisciDipendente(@RequestBody DipendenteDTO dto,
+	            @PathVariable Integer idAzienda,
+	            @PathVariable(required = false) Integer idPosto) {
 	        
-	        if (idPosto != null) {
+	        if (idPosto != null && idPosto > 0) {
 	            dipendenteService.inserisciDipendenteAssegnaPostoAuto(idAzienda, idPosto, dto);
-	            
 	        } else {
-	        	
 	            dipendenteService.inserisciDipendenteSenzaPostoAuto(idAzienda, dto);
 	        }
 	    }
-
+	    
+	    @PostMapping("/nuovoConPosto/{idAzienda}/{idPosto}")
+	    public void inserisciConPosto(@PathVariable Integer idAzienda, @PathVariable Integer idPosto, @RequestBody DipendenteDTO dto) {
+	        dipendenteService.inserisciDipendenteAssegnaPostoAuto(idAzienda, idPosto, dto);
+	    }
+	    
+	    @GetMapping("/mostraTuttiDip")
+	    public List<DipendenteDTO> visualizzaTutti() {
+	        return dipendenteService.visualizzaDipendenti();
+	    }
 	   
 	    @GetMapping(path = "/nomeCognDip", produces = "application/json")
 	    public List<DipendenteDTO> visualizzaNomiCognDipendenti() {
@@ -39,15 +53,38 @@ public class DipendenteController {
 	    }
 
 	   
-	    @DeleteMapping(path = "/cancellaDip/{matricola}", produces = "application/json")
-	    public DipendenteDTO cancellaImpiegatoMatricola(@PathVariable Integer matricola) {
+	    @DeleteMapping(path = "/cancellaDipMatrBase/{matricola}", produces = "application/json")
+	    public DipendenteDTO cancellaImpiegatoMatricola(@PathVariable String matricola) {
 	        return dipendenteService.cancellaImpiegatoMatricola(matricola);
+	    }
+	    
+	    @DeleteMapping("/cancellaDipMatrNomCogn/{matricola}")
+	    public DipendenteDTO cancellaPerMatricola(@PathVariable String matricola) {
+	 
+	        return dipendenteService.cancellaImpiegatoMatricola(matricola);
+	    }
+	    
+	    
+	    @PutMapping("/spostaImpAzi/{matricola}/{idNuovaAzienda}")
+	    public DipendenteDTO spostaImpiegato(@PathVariable String matricola, @PathVariable Integer idNuovaAzienda) {
+	        return dipendenteService.spostaImpiegatoAzienda(matricola, idNuovaAzienda);
 	    }
 
 	    
-	    @PutMapping(path = "/modificaSalario/{matricola}/{nuovoSalario}", produces = "application/json")
-	    public DipendenteDTO modificaSalarioDipendente( @PathVariable Integer matricola, @PathVariable Double nuovoSalario) {
+	    @PutMapping(path = "/modificaSalarioDip/{matricola}/{nuovoSalario}", produces = "application/json")
+	    public DipendenteDTO modificaSalarioDipendente( @PathVariable String matricola, @PathVariable Double nuovoSalario) {
 	        return dipendenteService.modificaSalarioDipendente(matricola, nuovoSalario);
 	    }
+	    
+	    @PutMapping("/modificaPosto/{matricola}/{idNuovoPosto}")
+	    public DipendenteDTO modificaPosto(@PathVariable String matricola, @PathVariable Integer idNuovoPosto) {
+	        return dipendenteService.modificaPostoAuto(matricola, idNuovoPosto);
+	    }
+
+	    @GetMapping("/visualizzaPostoEsi/{matricola}")
+	    public DipendenteDTO visualizzaPosto(@PathVariable String matricola) {
+	        return dipendenteService.visualizzaSeEsistePostoAuto(matricola);
+	    }
+	    
 	}
 
