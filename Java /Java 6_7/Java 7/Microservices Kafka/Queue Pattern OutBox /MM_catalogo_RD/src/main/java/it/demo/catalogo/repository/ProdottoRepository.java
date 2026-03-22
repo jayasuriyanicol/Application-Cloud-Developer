c22a68825d6bc6a766d813bea1da44a13514bb2c
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2026-03-22 20:19:39 +0100

    REMOVE : Incorrect file, need it to change

--- Java /Java 6_7/Java 7/Microservices Kafka/Queue Pattern OutBox /MM_catalogo_RD/src/main/java/it/demo/catalogo/repository/ProdottoRepository.java
+++
@@ -1,9 +0,0 @@
-package it.demo.catalogo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import it.demo.catalogo.entity.Prodotto;
-
-public interface ProdottoRepository extends JpaRepository<Prodotto, Integer> {
-
-}
