bc1e6fb9c2de602ab6ca15d19cfe13056b6989f9
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2026-03-27 13:06:18 +0100

    FEAT: Including the Service of Catalogo -  StreamBridge
    Adding the Service Catalogo to include a message of confirm od creation of the product.

---
+++ Java /Java 6_7/Java 7/Microservices Kafka/Queue Kafka & REST Exercise/Task 2 (Asincrona)/MM_catalogo_WR_Stream/src/main/java/it/demo/catalogo/service/CatalogService.java
@@ -0,0 +1,24 @@
+
+
+
+@Service 
+public class CatalogService {
+
+    @Autowired 
+    private ProdottoRepossitory repossitory;
+
+    @Autowired 
+    private StreamBridge bridge;
+
+   @Transactional 
+   public vodi creaProdotto(Prodotto p){
+
+        repository.save(p);
+
+        bridge.send("prodotto-stream-catalog", p);
+
+        System.out.println("SUCCESSO ! Il prodotto è stato creato correttamente ed è stato inviato a KAFKA come coda a messaggio");
+   } 
+
+
+}
