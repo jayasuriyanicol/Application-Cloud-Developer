e0341bdffdd00638613435ee52d310fa6e54b3f6
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2026-03-28 19:45:51 +0100

    FEAT:  Indirizzo with all annotation logic on it, requested from the prompt

---
+++ Java /Java 6_7/Java 7/Microservices Kafka/CC Bank Account Application /SpringCCBankAccount/src/main/java/com/spring/cc/entity/Indirizzo.java
@@ -0,0 +1,91 @@
+package com.spring.cc.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Indirizzo {
+	
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	private Integer idIndirizzo;
+	
+	@Column(nullable = false)
+	private String via;
+	
+	//Because there are some cases CAP with char + Numbers
+	@Column(nullable = false)
+	private String cap; 
+	
+	@Column(nullable = false)
+	private String città;
+	
+	@Column(nullable = false)
+	private String provincia;
+	
+	
+	public Indirizzo() {}
+	
+	public Indirizzo(Integer idIndirizzo, String via, String cap, String città, String provincia) {
+
+		this.idIndirizzo = idIndirizzo;
+		this.via = via;
+		this.cap = cap;
+		this.città = città;
+		this.provincia = provincia;
+	}
+
+
+	public Integer getIdIndirizzo() {
+	
+		
+		return idIndirizzo;
+	}
+	
+	
+	public void setIdIndirizzo(Integer idIndirizzo) {
+		this.idIndirizzo = idIndirizzo;
+	}
+	
+	public String getVia() {
+		return via;
+	}
+	
+	public void setVia(String via) {
+		this.via = via;
+	}
+	
+	public String getCap() {
+		return cap;
+	}
+	
+	public void setCap(String cap) {
+		this.cap = cap;
+	}
+	
+	public String getCittà() {
+		return città;
+	}
+	
+	public void setCittà(String città) {
+		this.città = città;
+	}
+	
+	public String getProvincia() {
+		return provincia;
+	}
+	
+	public void setProvincia(String provincia) {
+		this.provincia = provincia;
+	}
+	
+	
+	
+	
+	
+	
+
+}
