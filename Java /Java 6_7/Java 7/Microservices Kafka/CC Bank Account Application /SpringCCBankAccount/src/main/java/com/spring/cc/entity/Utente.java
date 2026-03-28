8ca2d2b513e4ba30abd8e74dfab30f9ea93ff82e
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2026-03-28 19:45:21 +0100

    FEAT:  Utente divided into 'intestatario & cointestatario' with all annotation logic on it, requested from the prompt

---
+++ Java /Java 6_7/Java 7/Microservices Kafka/CC Bank Account Application /SpringCCBankAccount/src/main/java/com/spring/cc/entity/Utente.java
@@ -0,0 +1,121 @@
+package com.spring.cc.entity;
+
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+
+@Entity
+public class Utente {
+	
+	@Id 
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer idUtente;
+	
+	@Column(nullable = false)
+	private String nome;
+	
+
+	@Column(nullable = false)
+	private String cognome;
+	
+	//Not orphan value, associated to 'Indirizzo'
+	@OneToOne(cascade = CascadeType.ALL, optional= false)
+	@JoinColumn(name= "id_indirizzo")
+	private Indirizzo indirizzo;
+	
+	@Column(nullable = false, unique= true)
+	private String mail; 
+	
+	//Attribute not need it
+	private String telefono;
+	
+	@OneToMany(mappedBy="intestatario")
+	private List<ContoCorrente> listaContiIntestati;
+	
+	@OneToMany(mappedBy="cointestatario")
+	private List<ContoCorrente> listaContiCointestati;
+	
+	public Utente () {}
+	
+	public Utente(Integer idUtente, String nome, String cognome, String mail, String telefono, Indirizzo indirizzo) {
+		
+		this.idUtente = idUtente;
+		this.nome = nome;
+		this.indirizzo = indirizzo;
+		this.cognome = cognome;
+		this.mail = mail;
+		this.telefono = telefono;
+	}
+
+
+	public Integer getIdUtente() {
+		return idUtente;
+	}
+
+
+	public void setIdUtente(Integer idUtente) {
+		this.idUtente = idUtente;
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+	public String getCognome() {
+		return cognome;
+	}
+
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+
+	public String getMail() {
+		return mail;
+	}
+
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	
+	
+	
+	
+	
+}	
+
+	
+	
+	
+	
+	
+	
+	
+
