b82b1f0e8f7a15eb9e5cc93205b1bcd92be12888
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2026-03-28 19:43:28 +0100

    FEAT:  Creation of the ApplicationSpring CC Bank Account

---
+++ Java /Java 6_7/Java 7/Microservices Kafka/CC Bank Account Application /SpringCCBankAccount/src/main/java/com/spring/cc/SpringCcBankAccountApplication.java
@@ -0,0 +1,13 @@
+package com.spring.cc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCcBankAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCcBankAccountApplication.class, args);
+	}
+
+}
