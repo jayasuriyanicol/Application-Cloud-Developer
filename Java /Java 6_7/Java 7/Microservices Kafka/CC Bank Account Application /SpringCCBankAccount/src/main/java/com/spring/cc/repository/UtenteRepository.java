ab2f82917bd8a3ca6bd687fd5f2dc66298e0b63b
jayasuriyanicol <jayasuriyanicol28@gmail.com>
2026-04-09 17:16:08 +0200

    FIX: Minor fix adding the '@Repository' forgotten

--- Java /Java 6_7/Java 7/Microservices Kafka/CC Bank Account Application /SpringCCBankAccount/src/main/java/com/spring/cc/repository/UtenteRepository.java
+++ Java /Java 6_7/Java 7/Microservices Kafka/CC Bank Account Application /SpringCCBankAccount/src/main/java/com/spring/cc/repository/UtenteRepository.java
@@ -1,9 +1,11 @@
 package com.spring.cc.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.spring.cc.entity.Utente;
+import org.springframework.stereotype.Repository;
 
+
+@Repository
 public interface UtenteRepository extends JpaRepository<Utente, Integer>{
 	
     // ? Verifying if an email already exists in the database (logic business)
