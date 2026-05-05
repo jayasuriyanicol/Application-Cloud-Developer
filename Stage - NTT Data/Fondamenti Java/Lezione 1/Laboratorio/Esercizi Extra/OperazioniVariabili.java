4d3d00d5382ceff32e9763ff9b7f65eb20c931a7
Jayasuriya Nicol <jayasuriyanicol28@gmail.com>
2026-05-05 21:11:40 +0200

    FEAT: the last two 'extra' exercises as review to practise on Java Fondaments

---
+++ Stage - NTT Data/Fondamenti Java/Lezione 1/Laboratorio/Esercizi Extra/OperazioniVariabili.java
@@ -0,0 +1,19 @@
+package Lezione_01;
+
+public class OperazioniVariabili {
+	
+	public static void main(String[] args) {
+		
+		Integer a = 20;
+		Integer b = 4;
+		
+		System.out.println("A VALORE ->"+ a);
+		System.out.println("B VALORE ->"+ b);
+		System.out.println("SOMMA ->"+ (a+b));
+		System.out.println("DIFFERENZA ->"+ (a-b));
+		System.out.println("PRODOTTO ->"+ (a*b));
+		System.out.println("DIVISIONE ->"+ (a/b));
+		
+	}
+
+}
