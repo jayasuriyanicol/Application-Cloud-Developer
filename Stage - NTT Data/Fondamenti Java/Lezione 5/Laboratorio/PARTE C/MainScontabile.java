f18f13a7134e03100343f60e4c9e6aab95b11126
Jayasuriya Nicol <jayasuriyanicol28@gmail.com>
2026-05-11 14:24:21 +0200

    FIX: Included the informative comment about the usage of String.format & the printf.

--- Stage - NTT Data/Fondamenti Java/Lezione 5/Laboratorio/PARTE C/MainScontabile.java
+++ Stage - NTT Data/Fondamenti Java/Lezione 5/Laboratorio/PARTE C/MainScontabile.java
@@ -8,6 +8,8 @@ public class MainScontabile {
 		ProdottoFisico pf = new ProdottoFisico();
 		ProdottoDigitale pd = new ProdottoDigitale();
 		
+		// ? Here we using a format method, the first one is calling the String.format
+		// ? then in the second one we gonna apply the printf method, with a simple format
 		System.out.println(String.format("%.2f",pf.applicaSconto(0.10)));
 		System.out.printf("%.2f\n", pd.applicaSconto(0.20));
 	}
